1d4027a38c33fba6a7cd9f1a928de2f03bd095ab
Simon Gauvin <simon.gauvin@polymtl.ca>
2020-01-19 16:53:39 -0500

    Add Angular Material to project

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule, FormsModule],
+    imports: [BrowserModule, HttpClientModule, FormsModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
