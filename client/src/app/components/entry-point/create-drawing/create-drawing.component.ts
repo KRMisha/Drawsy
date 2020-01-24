b834ff3122342ac4629cab22fb2fb2c05d881f60
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-01-24 16:40:48 -0500

    Add create-drawing component

---
+++ client/src/app/components/entry-point/create-drawing/create-drawing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-drawing',
+  templateUrl: './create-drawing.component.html',
+  styleUrls: ['./create-drawing.component.scss']
+})
+export class CreateDrawingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
