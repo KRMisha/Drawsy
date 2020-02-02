be6f7999dd83e30f885ded4fb3095b50a2b8e602
Francis Pepin <francis.pepin@polymtl.ca>
2020-02-02 13:29:13 -0500

    Add functional svg pencil using path

--- client/src/app/components/drawing/drawing.component.ts
+++ client/src/app/components/drawing/drawing.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { PencilService } from '../../services/pencil/pencil.service'
 
 @Component({
     selector: 'app-drawing',
@@ -7,18 +8,17 @@ import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
 })
 export class DrawingComponent {
     private isMouseDown = false;
-    private lastMouseX: number;
-    private lastMouseY: number;
-    
-    private svg = this.renderer.createElement('svg', 'svg');
-    // private path: any;
-    // private pathAttribute: string;
+    private pathString: string;
+    private svg: SVGElement = this.renderer.createElement('svg', 'svg');
+    private path: SVGPathElement;
 
-    constructor(private renderer: Renderer2, private element: ElementRef) {
+    constructor(private renderer: Renderer2, private element: ElementRef,
+                private pencilService: PencilService) {
         this.renderer.setAttribute(this.svg, 'version', '1.1');
         this.renderer.setAttribute(this.svg, 'baseProfile', 'full');
         this.renderer.setAttribute(this.svg, 'width', '100%');
         this.renderer.setAttribute(this.svg, 'height', '100%');
+        this.renderer.setAttribute(this.svg, 'stroke', 'white');
         this.renderer.setAttribute(this.svg, 'version', 'http://www.w3.org/2000/svg');
         this.renderer.appendChild(this.element.nativeElement, this.svg);
     }
@@ -26,14 +26,19 @@ export class DrawingComponent {
     @HostListener('mousedown', ['$event'])
     onMouseDown(event: MouseEvent) {
         this.isMouseDown = true;
-        this.lastMouseX = event.clientX - this.element.nativeElement.offsetLeft;
-        this.lastMouseY = event.clientY;
 
-        // this.pathAttribute = `M${this.lastMouseX} ${this.lastMouseY} `;
-        // this.path = this.renderer.createElement('path', 'svg')
-        // this.renderer.setAttribute(this.path, 'stroke', 'black');
-        // this.renderer.setAttribute(this.path, 'stroke-width', '5');
-        // this.renderer.appendChild(this.svg, this.path);
+        this.path = this.renderer.createElement('path', 'svg');
+        this.renderer.setAttribute(this.path, 'stroke', 'black');
+        this.renderer.setAttribute(this.path, 'fill', 'none');
+        this.renderer.setAttribute(this.path, 'stroke-width', '5');
+        this.renderer.setAttribute(this.path, 'stroke-linecap', 'round');
+        this.renderer.setAttribute(this.path, 'stroke-linejoin', 'round');
+
+        this.pathString = this.pencilService.
+        pathBegin(event.clientX - this.element.nativeElement.offsetLeft, event.clientY);
+
+        this.renderer.setAttribute(this.path, 'd', this.pathString);
+        this.renderer.appendChild(this.svg, this.path);
     }
 
     @HostListener('mouseup', ['$event'])
@@ -44,30 +49,11 @@ export class DrawingComponent {
     @HostListener('mousemove', ['$event'])
     onClick(event: MouseEvent): void {
         if (this.isMouseDown) {
-            // const rect = this.renderer.createElement('rect', 'svg');
-            // this.renderer.setAttribute(rect, 'width', '20');
-            // this.renderer.setAttribute(rect, 'height', '20');
-            // this.renderer.setAttribute(rect, 'background-color', 'blue');
-            // this.renderer.setAttribute(rect, 'x', `${event.clientX - this.element.nativeElement.offsetLeft}`);
-            // this.renderer.setAttribute(rect, 'y', `${event.clientY}`);
-            // this.renderer.appendChild(this.svg, rect);
+            this.pathString += this.pencilService.
+            pathLine(event.clientX - this.element.nativeElement.offsetLeft, event.clientY);
 
-            const line = this.renderer.createElement('line', 'svg');
-            this.renderer.setAttribute(line, 'x1', `${this.lastMouseX}`);
-            this.renderer.setAttribute(line, 'x2', `${event.clientX - this.element.nativeElement.offsetLeft}`);
-            this.renderer.setAttribute(line, 'y1', `${this.lastMouseY}`);
-            this.renderer.setAttribute(line, 'y2', `${event.clientY}`);
-            this.renderer.setAttribute(line, 'stroke', 'black');
-            this.renderer.setAttribute(line, 'stroke-width', '3');
-            this.renderer.appendChild(this.svg, line);
-
-            // this.pathAttribute += 
-                // `M${event.clientX - this.element.nativeElement.offsetLeft - this.lastMouseX} ${event.clientY - this.lastMouseY} `;
-
-            // this.renderer.setAttribute(this.path, 'd', `${this.pathAttribute}`);
-
-            this.lastMouseX = event.clientX - this.element.nativeElement.offsetLeft;
-            this.lastMouseY = event.clientY;
+            this.renderer.setAttribute(this.path, 'd', this.pathString);
+            this.renderer.appendChild(this.svg, this.path);
         }
     }
 }
