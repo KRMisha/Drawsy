28633b514152f7cdee1f0ab27b08ab1aab5a47fb
Francis Pepin <francis.pepin@polymtl.ca>
2020-02-05 21:22:27 -0500

    Add brush, line and rectangle tool services

---
+++ client/src/app/services/drawing/tools/tool-line/tool-line.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ToolLineService } from './tool-line.service';
+
+describe('ToolLineService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ToolLineService = TestBed.get(ToolLineService);
+    expect(service).toBeTruthy();
+  });
+});
