925daf23000dcac1e35f5a15a7cab99825ade8b1
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-02-06 22:16:36 -0500

    Add submit to service

--- client/src/app/components/create-drawing/create-drawing.component.ts
+++ client/src/app/components/create-drawing/create-drawing.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
-<<<<<<< Updated upstream
-import { MatDialogRef } from '@angular/material/dialog';
-import { Color } from '../../classes/color/color';
-import { ColorService } from '../../services/color/color.service';
-import { CreateDrawingService } from '../../services/create-drawing/data-sharer/create-drawing.service';
-=======
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog'
 import { CreateDrawingService } from 'src/app/services/create-drawing/create-drawing.service';
->>>>>>> Stashed changes
 
 @Component({
     selector: 'app-create-drawing',
@@ -16,38 +10,24 @@ import { CreateDrawingService } from 'src/app/services/create-drawing/create-dra
 })
 export class CreateDrawingComponent {
 
-<<<<<<< Updated upstream
-    constructor(
-        public dialogRef: MatDialogRef<CreateDrawingComponent>,
-        private drawingService: CreateDrawingService,
-        private colorService: ColorService,
-    ) {
-        // this.drawingService.changeColor(new Color(255, 255, 255, 1));
-        this.drawingService.changeHeight(0); // TODO: set to window size
-        this.drawingService.changeWidth(0); // TODO: set to window size
-
-        drawingService.width$.subscribe(width => (this.drawingWidth = width));
-        drawingService.height$.subscribe(height => (this.drawingHeight = height));
-        // drawingService.color$.subscribe(color => (this.drawingColor = color));
-    }
-
-    close(): void {
-        this.dialogRef.close();
-    }
-=======
     drawingForm = new FormGroup({
-        height: new FormControl('', Validators.compose([Validators.required, Validators.min(0), Validators.max(10000)]) ),
+        height: new FormControl('', Validators.compose([Validators.required, Validators.min(0), Validators.max(10000)])),
         width: new FormControl('', Validators.compose([Validators.required, Validators.min(0), Validators.max(10000)])),
     })
 
-    constructor(private drawingService: CreateDrawingService) {}
->>>>>>> Stashed changes
+    constructor(
+        public dialogRef: MatDialogRef<CreateDrawingComponent>,
+        private drawingService: CreateDrawingService,
+        ) {}
 
     onSubmit() {
-        //this.drawingService.changeHeight(this.drawingForm.);
+        this.drawingService.changeHeight(this.drawingForm.controls['height'].value);
+        this.drawingService.changeWidth(this.drawingForm.controls['width'].value);
+        // send the color
+        this.onClose();
     }
 
     onClose() {
-
+        this.dialogRef.close();
     }
 }
