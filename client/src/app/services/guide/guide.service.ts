f557a2bf43a6907d0f8b9831896efd25ec709da1
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-02-08 17:47:14 -0500

    Add guide file hierarchy

--- client/src/app/services/guide/guide.service.ts
+++ client/src/app/services/guide/guide.service.ts
@@ -1,14 +1,14 @@
 import { Injectable, Type } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 
-import { GuideBrushComponent } from '../../components/guide/guide-brush/guide-brush.component';
-import { GuideColorComponent } from '../../components/guide/guide-color/guide-color.component';
-import { GuideExportDrawingComponent } from '../../components/guide/guide-export-drawing/guide-export-drawing.component';
-import { GuideLineComponent } from '../../components/guide/guide-line/guide-line.component';
-import { GuidePencilComponent } from '../../components/guide/guide-pencil/guide-pencil.component';
-import { GuideRectangleComponent } from '../../components/guide/guide-rectangle/guide-rectangle.component';
-import { GuideSaveDrawingComponent } from '../../components/guide/guide-save-drawing/guide-save-drawing.component';
-import { GuideWelcomeComponent } from '../../components/guide/guide-welcome/guide-welcome.component';
+import { GuideBrushComponent } from '../../components/guide/guide-content/guide-tools/guide-brushes/guide-brush/guide-brush.component';
+import { GuideColorComponent } from '../../components/guide/guide-content/guide-tools/guide-color/guide-color.component';
+import { GuideExportDrawingComponent } from '../../components/guide/guide-content/guide-file-options/guide-export-drawing/guide-export-drawing.component';
+import { GuideLineComponent } from '../../components/guide/guide-content/guide-tools/guide-line/guide-line.component';
+import { GuidePencilComponent } from '../../components/guide/guide-content/guide-tools/guide-brushes/guide-pencil/guide-pencil.component';
+import { GuideRectangleComponent } from '../../components/guide/guide-content/guide-tools/guide-shapes/guide-rectangle/guide-rectangle.component';
+import { GuideSaveDrawingComponent } from '../../components/guide/guide-content/guide-file-options/guide-save-drawing/guide-save-drawing.component';
+import { GuideWelcomeComponent } from '../../components/guide/guide-content/guide-welcome/guide-welcome.component';
 
 const guides: Type<any>[] = [
     GuideWelcomeComponent,
