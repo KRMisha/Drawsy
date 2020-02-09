7081458d8d5f875a72fb3e140464c79aaf1a29ae
Francis Pepin <francis.pepin@polymtl.ca>
2020-02-09 15:21:36 -0500

    Rename boolean

--- client/src/app/services/modal/modal.service.spec.ts
+++ client/src/app/services/modal/modal.service.spec.ts
@@ -2,15 +2,12 @@ import { async, TestBed } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalService } from './modal.service';
 
-
 describe('ModalService', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        { provide: MatDialog, useValue: {} as MatDialog }]
-    })
-
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            providers: [{ provide: MatDialog, useValue: {} as MatDialog }],
+        });
+    }));
 
     it('should be created', () => {
         const service: ModalService = TestBed.get(ModalService);
