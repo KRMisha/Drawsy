61f5f1cdef5b9000aa6bf68eef1ccf2a6d6c0e43
Francis Pepin <francis.pepin@polymtl.ca>
2020-02-09 17:42:23 -0500

    Add mock providers to tool-holder-service testbed

--- client/src/app/services/drawing/tool-holder/tool-holder.service.spec.ts
+++ client/src/app/services/drawing/tool-holder/tool-holder.service.spec.ts
@@ -1,12 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
+import { ToolBrushService } from '../tools/tool-brush/tool-brush.service';
+import { ToolLineService } from '../tools/tool-line/tool-line.service';
+import { ToolPencilService } from '../tools/tool-pencil/tool-pencil.service';
+import { ToolRectangleService } from '../tools/tool-rectangle/tool-rectangle.service';
 import { ToolHolderService } from './tool-holder.service';
 
-describe('ToolHolderService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+class MockTool {};
+
+fdescribe('ToolHolderService', () => {
+    beforeEach(() => {
+        const mockToolPencilService = new MockTool();
+        const mockToolBrushService = new MockTool();
+        const mockToolLineService = new MockTool();
+        const mockToolRectangleService = new MockTool();
+        TestBed.configureTestingModule({
+            providers: [
+                {provide: ToolPencilService, useValue: mockToolPencilService},
+                {provide: ToolBrushService, useValue: mockToolBrushService},
+                {provide: ToolLineService, useValue: mockToolLineService},
+                {provide: ToolRectangleService, useValue: mockToolRectangleService},
+            ]
+        })
+    });
 
     it('should be created', () => {
         const service: ToolHolderService = TestBed.get(ToolHolderService);
         expect(service).toBeTruthy();
     });
+
+
 });
