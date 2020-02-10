373246f82d80a9cdabd3b245ced751db457e76b5
Francis Pepin <francis.pepin@polymtl.ca>
2020-02-09 19:53:07 -0500

    Add attribute testing to modal service

--- client/src/app/services/modal/modal.service.spec.ts
+++ client/src/app/services/modal/modal.service.spec.ts
@@ -1,16 +1,33 @@
+import { Type } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { Vec2 } from 'src/app/classes/vec2/vec2';
 import { ModalService } from './modal.service';
 
-describe('ModalService', () => {
+fdescribe('ModalService', () => {
+    let service: ModalService;
+    let matDialog: MatDialog;
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            providers: [{ provide: MatDialog, useValue: {} as MatDialog }],
+            providers: [{ provide: MatDialog, useValue: {} as MatDialog} }]
         });
+
+        service = TestBed.get(ModalService);
+        matDialog = TestBed.get(MatDialog);
+        spyOn(matDialog, 'open');
     }));
 
     it('should be created', () => {
-        const service: ModalService = TestBed.get(ModalService);
         expect(service).toBeTruthy();
     });
+
+    it('should return isModalPresent boolean attribute', () => {
+        expect(service.isModalPresent).toEqual(false);
+    })
+
+    it('should open default dialog if no dimensions are specified', async(() => {
+        service.openDialog({} as any);
+        expect(matDialog.open).toHaveBeenCalledWith([{}, {}]);
+    }))
 });
