eb23e03c4d62df2541034838801d025cbede3831
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-02-11 15:40:12 -0500

    Add tests on guide-sidebar-component

--- client/src/app/components/guide/guide-sidebar/guide-sidebar.component.spec.ts
+++ client/src/app/components/guide/guide-sidebar/guide-sidebar.component.spec.ts
@@ -21,4 +21,22 @@ describe('GuideSidebarComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('#toggleMenu should activate the good menu', () => {
+        // MenuSection.ToolShapes = 2
+        component.toggleMenu(2);
+        expect(component.isEachMenuExpanded).toEqual([false, false, true, false, false]);
+    });
+
+    it('#toggleMenu should close menu on second toggle', () => {
+        component.toggleMenu(1);
+        component.toggleMenu(4);
+        component.toggleMenu(1);
+        expect(component.isEachMenuExpanded).toEqual([false, false, false, false, true]);
+    });
+
+    it('#expandAllMenus should open all menu', () => {
+        component.expandAllMenus();
+        expect(component.isEachMenuExpanded).toEqual([true, true, true, true, true]);
+    });
 });
