5be7a9ce8f3bdc92f8d680053736d72ae98696f9
Samer Massaad <samer.massaad@polymtl.ca>
2020-02-11 00:20:22 -0500

    Add tests to entry-point component

--- client/src/app/components/entry-point/entry-point.component.spec.ts
+++ client/src/app/components/entry-point/entry-point.component.spec.ts
@@ -3,18 +3,33 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { ModalService } from 'src/app/services/modal/modal.service';
+import { CreateDrawingComponent } from '../create-drawing/create-drawing.component';
+import { GuideComponent } from '../guide/guide.component';
 import { EntryPointComponent } from './entry-point.component';
 
 describe('EntryPointComponent', () => {
     let component: EntryPointComponent;
     let fixture: ComponentFixture<EntryPointComponent>;
-
+    let mockModalService: ModalService;
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [EntryPointComponent],
             imports: [MatSidenavModule, MatCardModule, MatDialogModule],
+            providers: [
+                {
+                    provide: ModalService,
+                    useValue: {
+                        openDialog: () => {
+                            return;
+                        },
+                    },
+                },
+            ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
+
+        mockModalService = TestBed.get(ModalService);
     }));
 
     beforeEach(() => {
@@ -25,5 +40,17 @@ describe('EntryPointComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('#openCreateDrawing should forward request to Modal service', () => {
+        spyOn(mockModalService, 'openDialog');
+        component.openCreateDrawing();
+        expect(mockModalService.openDialog).toHaveBeenCalledWith(CreateDrawingComponent);
+    });
+
+    it('#openGuideModal should forward request to Modal service', () => {
+        spyOn(mockModalService, 'openDialog');
+        component.openGuideModal();
+        expect(mockModalService.openDialog).toHaveBeenCalledWith(GuideComponent, { x: 1920, y: 1080 });
     });
 });
