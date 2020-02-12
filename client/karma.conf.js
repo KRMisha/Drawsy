764b248cd4ca428cb437d6130c0a92dcb2552007
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-02-11 23:00:52 -0500

    Reenable single run for tests

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        singleRun: false,
+        singleRun: true,
         browsers: ['Chrome'],
         customLaunchers: {
             FirefoxHeadless: {
