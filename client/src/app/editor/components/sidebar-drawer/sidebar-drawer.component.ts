de60b744a91a12a02e53867ff241172661056461
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-03-03 01:06:02 -0500

    Change disable of input to reactive form disable

--- client/src/app/editor/components/sidebar-drawer/sidebar-drawer.component.ts
+++ client/src/app/editor/components/sidebar-drawer/sidebar-drawer.component.ts
@@ -38,7 +38,7 @@ export class SidebarDrawerComponent implements OnInit, OnDestroy {
 
     junctionSizeGroup = new FormGroup({
         junctionSize: new FormControl(
-            0,
+            {value: 0, disabled: true},
             Validators.compose([
                 Validators.required,
                 Validators.max(maximumJunctionSize),
@@ -84,6 +84,11 @@ export class SidebarDrawerComponent implements OnInit, OnDestroy {
     }
 
     setSetting(setting: ToolSetting, value: number | JunctionSettings | StrokeTypes | Textures): void {
+        if (setting === ToolSetting.JunctionSettings) {
+            (value as JunctionSettings).hasJunction ?
+                this.junctionSizeGroup.controls.junctionSize.enable() : this.junctionSizeGroup.controls.junctionSize.disable();
+        }
+
         this.toolSelectorService.setSetting(setting, value);
     }
 
