c35b05995d60059fd8907c3fe82ece3d3a85bd1a
Francis Pepin <francis.pepin@polymtl.ca>
2020-03-06 13:46:36 -0500

    Add default gallery svg drawings

--- client/src/app/gallery/components/gallery/gallery.component.ts
+++ client/src/app/gallery/components/gallery/gallery.component.ts
@@ -7,12 +7,9 @@ import { Component } from '@angular/core';
 })
 export class GalleryComponent {
     links: string[] = [
-        'https://svgsilh.com/svg/1435214.svg',
-        'https://svgsilh.com/svg/1431397.svg',
-        'https://svgsilh.com/svg/1435217.svg',
-        'https://svgsilh.com/svg/47935.svg',
-        'https://svgsilh.com/svg/1325107.svg',
-        'http://thecraftchop.com/files/images/darth_1.svg',
-        'https://upload.wikimedia.org/wikipedia/commons/c/cf/Ubuntu_alternative_background.svg',
+        './../../../../../assets/Drawings/Kirby.svg',
+        './../../../../assets/Drawings/Milk and wine.svg',
+        './../../../../assets/Drawings/Unicorn.svg',
+        './../../../../assets/Drawings/Windows logo.svg',
     ];
 }
