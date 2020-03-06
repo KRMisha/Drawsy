a53973239e359826be66b027b20e57ebe53d5d23
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-03-05 21:14:12 -0500

    Add reactive form validation

--- client/src/app/modals/components/export-drawing/export-drawing.component.ts
+++ client/src/app/modals/components/export-drawing/export-drawing.component.ts
@@ -1,9 +1,11 @@
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { PreviewFilter } from '@app/drawing/enums/preview-filter.enum';
 import { DrawingPreviewService } from '@app/drawing/services/drawing-preview.service';
 import { DrawingSerializerService } from '@app/drawing/services/drawing-serializer.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 export interface Label {
     name: string;
@@ -13,12 +15,37 @@ export interface Label {
     templateUrl: './export-drawing.component.html',
     styleUrls: ['./export-drawing.component.scss'],
 })
-export class ExportDrawingComponent {
+export class ExportDrawingComponent implements OnInit, OnDestroy{
     PreviewFilter = PreviewFilter; // Make enum available to template
+
+    labelFormSubscription: Subscription;
+
+    labelForm = new FormGroup({
+        label: new FormControl(
+            '',
+            [Validators.pattern('^([0-9a-zA-Z\ \-éèîêôçàû])*$'), Validators.required]
+        )
+    });
 
     readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
     constructor(private drawingSerializerService: DrawingSerializerService, private drawingPreviewService: DrawingPreviewService) {}
+
+    ngOnInit(): void {
+        this.labelForm.controls.label.setValue(this.drawingPreviewService.labels);
+
+        this.labelFormSubscription = this.labelForm.controls.label.valueChanges.subscribe(() => {
+            const tempLabels = this.labels;
+            if (this.labelForm.controls.label.valid) {
+                tempLabels.push(this.labelForm.controls.label.value);
+                this.labels = tempLabels;
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.labelFormSubscription.unsubscribe();
+    }
 
     exportDrawingAsSvg(): void {
         this.drawingPreviewService.finalizePreview();
@@ -39,8 +66,26 @@ export class ExportDrawingComponent {
         const input = event.input;
         const value = event.value;
 
+        const control = this.labelForm.controls.label;
+
         if ((value || '').trim()) {
-            this.drawingPreviewService.labels.push(value.trim());
+            control.setErrors(null);
+            const tempLabels = this.labels;
+            tempLabels.push(value.trim());
+            control.setValue(value);
+            if (control.valid) {
+                control.markAsDirty();
+                input.value = '';
+            }
+            else {
+                const index = this.labels.findIndex(value1 => value1 === value.trim());
+                if(index !== -1) {
+                    this.labels.splice(index, 1);
+                }
+            }
+        }
+        else {
+            this.labelForm.controls.label.updateValueAndValidity();
         }
 
         if (input !== undefined) {
@@ -51,9 +96,14 @@ export class ExportDrawingComponent {
     removeLabel(label: string): void {
         const index = this.labels.indexOf(label);
 
+        const control = this.labelForm.controls.label;
+
         if (index >= 0) {
-            this.drawingPreviewService.labels.splice(index, 1);
+            this.labels.splice(index, 1);
         }
+
+        control.updateValueAndValidity();
+        control.markAsDirty();
     }
 
     get title(): string {
