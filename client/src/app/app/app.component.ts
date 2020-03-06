58d04bbae5962245be348dc15cfe2b1151fec9e6
Francis Pepin <francis.pepin@polymtl.ca>
2020-03-05 23:56:08 -0500

    Add key shortcut to drawing gallery

--- client/src/app/app/app.component.ts
+++ client/src/app/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { GalleryComponent } from '@app/gallery/components/gallery/gallery.component';
 import { NewDrawingComponent } from '@app/modals/components/new-drawing/new-drawing.component';
 import { ModalService } from '@app/modals/services/modal.service';
 
@@ -20,6 +21,9 @@ export class AppComponent {
         if (event.ctrlKey && event.key === 'o') {
             event.preventDefault();
             this.modalService.openDialog(NewDrawingComponent, { x: 500, y: 500 });
+        } else if (event.ctrlKey && event.key === 'g') {
+            event.preventDefault();
+            this.modalService.openDialog(GalleryComponent, { x: 1920, y: 1000 });
         }
     }
 }
