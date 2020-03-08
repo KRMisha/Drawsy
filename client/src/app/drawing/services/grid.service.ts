9fd6bfefa85f5d92f299f5b53359ed3c608861e0
Samer Massaad <samer.massaad@polymtl.ca>
2020-03-07 23:43:13 -0500

    Add basic grid logic

--- client/src/app/drawing/services/grid.service.ts
+++ client/src/app/drawing/services/grid.service.ts
@@ -8,18 +8,35 @@ const gridSizeVariation = 5;
 export class GridService {
     gridSize = 50;
     isDisplayed = false;
+    private gridPatternElement: SVGPatternElement;
+    private gridPathElement: SVGPathElement;
+
+    setElementRoots(patternRoot: SVGPatternElement, pathRoot: SVGPathElement): void {
+        this.gridPatternElement = patternRoot;
+        this.gridPathElement = pathRoot;
+    }
 
     toggleGrid(): void {
         this.isDisplayed = !this.isDisplayed;
+        const visibility = this.isDisplayed ? 'visible' : 'hidden';
+        this.gridPathElement.setAttribute('visibility', visibility);
     }
 
     raiseGridSize(): void {
         this.gridSize += gridSizeVariation;
+        this.updateGridSize();
     }
 
     lowerGridSize(): void {
         if (this.gridSize - gridSizeVariation > 0) {
             this.gridSize -= gridSizeVariation;
+            this.updateGridSize();
         }
+    }
+
+    private updateGridSize(): void {
+        this.gridPathElement.setAttribute('d', `M ${this.gridSize} 0 L 0 0 0 100`);
+        this.gridPatternElement.setAttribute('width', this.gridSize.toString());
+        this.gridPatternElement.setAttribute('height', this.gridSize.toString());
     }
 }
