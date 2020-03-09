0da48588f3730e72bb24c1dfcb44e77358968129
Samer Massaad <samer.massaad@polymtl.ca>
2020-03-09 03:14:38 -0400

    Fix append-element-command tests

--- client/src/app/drawing/classes/commands/append-element-command.spec.ts
+++ client/src/app/drawing/classes/commands/append-element-command.spec.ts
@@ -1,7 +1,31 @@
+import { DrawingService } from '@app/drawing/services/drawing.service';
 import { AppendElementCommand } from './append-element-command';
 
 describe('AppendElementCommand', () => {
+    let command: AppendElementCommand;
+    let drawingService: DrawingService;
+    let svgElement: SVGElement;
+
+    beforeEach(() => {
+        svgElement = {} as unknown as SVGElement;
+        drawingService = {
+            removeElement: (element: SVGElement) => {return;},
+            addElement: (element:SVGElement) => {return;}
+        } as DrawingService;
+        command = new AppendElementCommand(drawingService, svgElement);
+    })
+
     it('should create an instance', () => {
-        expect(new AppendElementCommand()).toBeTruthy();
+        expect(command).toBeTruthy();
     });
+
+    it('#undo should forward removeElement calling to drawingService', () => {
+        spyOn(drawingService, 'removeElement').and.stub();
+        expect(drawingService).toHaveBeenCalledWith(svgElement);
+    })
+
+    it('#redo should forward addElement calling to drawingService', () => {
+        spyOn(drawingService, 'addElement').and.stub();
+        expect(drawingService).toHaveBeenCalledWith(svgElement);
+    })
 });
