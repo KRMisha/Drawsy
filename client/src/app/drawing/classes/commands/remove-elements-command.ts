2783a9c2224e663acb5a620d5607130b8677f34a
Simon Gauvin <simon.gauvin@polymtl.ca>
2020-03-09 23:18:06 -0400

    Fix undo redo element order with eraser

--- client/src/app/drawing/classes/commands/remove-elements-command.ts
+++ client/src/app/drawing/classes/commands/remove-elements-command.ts
@@ -1,18 +1,19 @@
 import { Command } from '@app/drawing/classes/commands/command';
 import { DrawingService } from '@app/drawing/services/drawing.service';
+import { ElementAndItsNeighbour } from '../element-and-its-neighbour';
 
 export class RemoveElementsCommand implements Command {
-    constructor(private drawingService: DrawingService, private elements: SVGElement[]) {}
+    constructor(private drawingService: DrawingService, private elements: ElementAndItsNeighbour[]) {}
 
     undo(): void {
         for (const element of this.elements) {
-            this.drawingService.addElement(element);
+            this.drawingService.addElement(element.element, element.neighbour);
         }
     }
 
     redo(): void {
         for (const element of this.elements) {
-            this.drawingService.removeElement(element);
+            this.drawingService.removeElement(element.element);
         }
     }
 }
