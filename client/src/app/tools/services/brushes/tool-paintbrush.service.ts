e235ad3ced68b0214962074f6f95e1c34f74d4c1
Samer Massaad <samer.massaad@polymtl.ca>
2020-03-09 23:57:27 -0400

    Fix default setting value imports

--- client/src/app/tools/services/brushes/tool-paintbrush.service.ts
+++ client/src/app/tools/services/brushes/tool-paintbrush.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ColorService } from '@app/drawing/services/color.service';
 import { CommandService } from '@app/drawing/services/command.service';
 import { DrawingService } from '@app/drawing/services/drawing.service';
-import { defaultTexture } from '@app/tools/enums/tool-defaults.enum';
+import ToolDefaults from '@app/tools/enums/tool-defaults';
 import { ToolName } from '@app/tools/enums/tool-name.enum';
 import { ToolSetting } from '@app/tools/enums/tool-settings.enum';
 import { ToolBrush } from '@app/tools/services/brushes/tool-brush';
@@ -13,7 +13,7 @@ import { ToolBrush } from '@app/tools/services/brushes/tool-brush';
 export class ToolPaintbrushService extends ToolBrush {
     constructor(protected drawingService: DrawingService, colorService: ColorService, protected commandService: CommandService) {
         super(drawingService, colorService, commandService, ToolName.Brush);
-        this.toolSettings.set(ToolSetting.Texture, defaultTexture);
+        this.toolSettings.set(ToolSetting.Texture, ToolDefaults.defaultTexture);
     }
 
     protected createNewPath(): SVGPathElement {
