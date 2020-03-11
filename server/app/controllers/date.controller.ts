25de9081666d4e75d1b1527b05183a5dc2d9d423
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-03-11 00:06:26 -0400

    Remove unnecessary server imports from framework

--- server/app/controllers/date.controller.ts
+++ server/app/controllers/date.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response, Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { inject, injectable } from 'inversify';
 import { Message } from '../../../common/communication/message';
 import { DateService } from '../services/date.service';
--- server/app/services/index.service.ts
+++ server/app/services/index.service.ts
@@ -1,5 +1,4 @@
 import { inject, injectable } from 'inversify';
-import 'reflect-metadata';
 import { Message } from '../../../common/communication/message';
 import Types from '../types';
 import { DateService } from './date.service';
