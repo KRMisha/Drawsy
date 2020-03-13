38616cc17ff419eab76bf639b530c4235cf3142d
Simon Gauvin <simon.gauvin@polymtl.ca>
2020-03-13 09:52:20 -0400

    Fix regressions

---
+++ client/src/app/tools/services/shapes/tool-shape.spec.ts
@@ -0,0 +1,7 @@
+// import { ToolShape } from './tool-shape';
+
+describe('ToolShape', () => {
+    // it('should create an instance', () => {
+    //     expect(new ToolShape()).toBeTruthy();
+    // });
+});
