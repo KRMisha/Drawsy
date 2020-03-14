68482a2b3993b0138ea13f24d7c6a8ba05fcfa67
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-03-14 21:50:10 +0000

    Apply suggestion to client/src/app/tools/services/shapes/tool-shape.spec.ts

--- client/src/app/tools/services/shapes/tool-shape.spec.ts
+++ client/src/app/tools/services/shapes/tool-shape.spec.ts
@@ -12,7 +12,9 @@ import { DrawingService } from '@app/drawing/services/drawing.service';
 // import { ToolSetting } from '@app/tools/enums/tool-settings.enum';
 // import { Tool } from '@app/tools/services/tool';
 // import { ToolShape } from './tool-shape';
-
+import { ColorService } from '@app/drawing/services/color.service';
+import { CommandService } from '@app/drawing/services/command.service';
+import { DrawingService } from '@app/drawing/services/drawing.service';
 describe('Shape', () => {
     let drawingServiceSpyObj: jasmine.SpyObj<DrawingService>;
     let colorService: ColorService;
