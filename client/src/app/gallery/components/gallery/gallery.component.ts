dc2f33201851e5bef46ade3e2cebd36c0fb2b7b9
Samer Massaad <samer.massaad@polymtl.ca>
2020-03-14 21:45:21 -0400

    Fix new drawing warning duplication

--- client/src/app/gallery/components/gallery/gallery.component.ts
+++ client/src/app/gallery/components/gallery/gallery.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { SvgFileContainer } from '@app/classes/svg-file-container';
 import { DrawingSerializerService } from '@app/drawing/services/drawing-serializer.service';
-import { DrawingService } from '@app/drawing/services/drawing.service';
 import { ServerService } from '@app/server/services/server.service';
 import { SavedFile } from '../../../../../../common/communication/saved-file';
 import { descRegex } from '../../../../../../common/validation/desc-regex';
@@ -18,21 +18,21 @@ const maxInputStringLength = 15;
     styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
-    containers: SvgFileContainer[] = [];
+    drawings: SvgFileContainer[] = [];
     searchLabels: string[] = [];
     isLoaded = false;
 
-    galleryFormGroup = new FormGroup({
-        labelForm: new FormControl('', [Validators.pattern(descRegex), Validators.maxLength(maxInputStringLength)]),
+    galleryGroup = new FormGroup({
+        labels: new FormControl('', [Validators.pattern(descRegex), Validators.maxLength(maxInputStringLength)]),
     });
 
     readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
     constructor(
+        private router: Router,
         private drawingSerializerService: DrawingSerializerService,
         private serverService: ServerService,
         private snackBar: MatSnackBar,
-        private drawingService: DrawingService,
     ) {}
 
     ngOnInit(): void {
@@ -42,7 +42,7 @@ export class GalleryComponent implements OnInit {
     addLabel(event: MatChipInputEvent): void {
         const input = event.input;
         const value = event.value;
-        const control = this.galleryFormGroup.controls.labelForm;
+        const control = this.galleryGroup.controls.labels;
 
         if ((value || '').trim()) {
             control.setErrors(null);
@@ -61,7 +61,7 @@ export class GalleryComponent implements OnInit {
 
     removeLabel(label: string): void {
         const index = this.searchLabels.indexOf(label, 0);
-        const control = this.galleryFormGroup.controls.labelForm;
+        const control = this.galleryGroup.controls.labels;
 
         if (index >= 0) {
             this.searchLabels.splice(index, 1);
@@ -70,63 +70,53 @@ export class GalleryComponent implements OnInit {
         control.markAsDirty();
     }
 
-    hasSearchLabel(container: SvgFileContainer): boolean {
+    hasSearchLabel(drawing: SvgFileContainer): boolean {
         if (this.searchLabels.length === 0) {
             return true;
         }
 
-        for (const label of container.labels) {
-            for (const searchLabel of this.searchLabels) {
-                if (label === searchLabel) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return drawing.labels.some((label: string) => this.searchLabels.includes(label));
     }
 
-    deleteDrawing(selectedContainer: SvgFileContainer): void {
+    deleteDrawing(selectedDrawing: SvgFileContainer): void {
         const confirmationMessage =
-            'Attention! La suppression du dessin est irréversible. ' + 'Désirez-vous quand même supprimer le dessin?';
+            'Attention! La suppression du dessin est irréversible. Désirez-vous quand même supprimer le dessin?';
         if (!confirm(confirmationMessage)) {
             return;
         }
-        this.serverService.deleteDrawing(selectedContainer).subscribe(() => {
-            this.getAllDrawings();
+
+        this.serverService.deleteDrawing(selectedDrawing.id).subscribe(() => {
+            this.getAllDrawings(); // TODO: move to gallery service
         });
-        this.snackBar.open(`Dessin supprimé : ${selectedContainer.title}`, undefined, {
+        this.snackBar.open(`Dessin supprimé : ${selectedDrawing.title}`, undefined, {
             duration: 4000,
         });
     }
 
-    loadDrawing(selectedContainer: SvgFileContainer): void {
-        const confirmationMessage =
-            'Attention! Un dessin non-vide est déjà présent sur la zone de travail. ' +
-            'Désirez-vous charger le dessin et abandonner vos changements?';
-        if (this.drawingService.isDrawingStarted() && !confirm(confirmationMessage)) {
-            return;
+    loadDrawing(selectedDrawing: SvgFileContainer): void {
+        if (this.drawingSerializerService.loadSvgDrawing(selectedDrawing)) {
+            this.snackBar.open(`Dessin chargé : ${selectedDrawing.title}`, undefined, {
+                duration: 4000,
+            });
+            this.router.navigate(['/editor']);
         }
-        this.drawingSerializerService.loadSvgDrawing(selectedContainer);
-        this.snackBar.open(`Dessin chargé : ${selectedContainer.title}`, undefined, {
-            duration: 4000,
-        });
+    }
+
+    getLabelError(): string {
+        return this.galleryGroup.controls.labels.hasError('pattern')
+            ? '(A-Z, a-z, 0-9) uniquement'
+            : this.galleryGroup.controls.labels.hasError('maxlength')
+            ? 'Longueur maximale 15 caractères'
+            : '';
     }
 
     private getAllDrawings(): void {
         this.serverService.getAllDrawings().subscribe((savedFiles: SavedFile[]): void => {
-            this.containers = [];
+            this.drawings = [];
             for (const savedFile of savedFiles) {
-                this.containers.push(this.drawingSerializerService.convertSavedFileToSvgFileContainer(savedFile));
+                this.drawings.push(this.drawingSerializerService.convertSavedFileToSvgFileContainer(savedFile));
             }
             this.isLoaded = true;
         });
-    }
-
-    protected getLabelError(): string {
-        return this.galleryFormGroup.controls.labelForm.hasError('pattern')
-            ? '(A-Z, a-z, 0-9) uniquement'
-            : this.galleryFormGroup.controls.labelForm.hasError('maxlength')
-            ? 'Longueur maximale 15 caractères'
-            : '';
     }
 }
