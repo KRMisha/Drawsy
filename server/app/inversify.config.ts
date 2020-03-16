2397ab736d49e87d32b4d210bb0bb67866ab2a50
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-03-16 15:47:13 -0400

    Move polyfill to inversify config file for compatibility with tests

--- server/app/inversify.config.ts
+++ server/app/inversify.config.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { Container } from 'inversify';
 import { DatabaseController } from './controllers/database.controller';
 import { Application } from './server/app';
@@ -5,12 +6,10 @@ import { Server } from './server/server';
 import { DatabaseService } from './services/database.service';
 import Types from './types';
 
-const container: Container = new Container();
+export const container: Container = new Container();
 
 container.bind<Server>(Types.Server).to(Server);
 container.bind<Application>(Types.Application).to(Application);
 
 container.bind<DatabaseController>(Types.DatabaseController).to(DatabaseController);
 container.bind<DatabaseService>(Types.DatabaseService).to(DatabaseService);
-
-export { container };
