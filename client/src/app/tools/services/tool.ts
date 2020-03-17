ec43a6b5ba9a8a403f21185938b63201dbd0661d
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-03-17 15:01:01 -0400

    Fix English

--- client/src/app/tools/services/tool.ts
+++ client/src/app/tools/services/tool.ts
@@ -28,7 +28,7 @@ export abstract class Tool {
         this.name = name;
     }
 
-    // Deactivate tslint for method stubs below because not all derived service classes
+    // Disable tslint for method stubs below because not all derived service classes
     // may need to override the functionality and would needlessly define no-ops otherwise
     // tslint:disable: no-empty
     afterDrawingInit(): void {}
