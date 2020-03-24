f85558d310a44807122dda6a63fe1208eb2a87b1
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-03-24 02:58:29 -0400

    Refactor sidebar button interfaces

---
+++ client/src/app/editor/classes/action-button.ts
@@ -0,0 +1,5 @@
+export interface ActionButton {
+    name: string;
+    icon: string;
+    action: () => void;
+}
--- client/src/app/editor/classes/sidebar-button.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SidebarButton } from '@app/editor/classes/sidebar-button';
-
-describe('SidebarButton', () => {
-    it('should create an instance', () => {
-        expect(new SidebarButton()).toBeTruthy();
-    });
-});
