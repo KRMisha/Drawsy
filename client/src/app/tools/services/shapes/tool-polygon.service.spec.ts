f345e9070e3d7183b0e6496e247c4e9e47ad5cd9
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-03-24 18:19:42 -0400

    Add test for multiple side counts in tool-polygon

--- client/src/app/tools/services/shapes/tool-polygon.service.spec.ts
+++ client/src/app/tools/services/shapes/tool-polygon.service.spec.ts
@@ -8,7 +8,7 @@ import { DrawingService } from '@app/drawing/services/drawing.service';
 import { ToolSetting } from '@app/tools/enums/tool-setting.enum';
 import { ToolPolygonService } from '@app/tools/services/shapes/tool-polygon.service';
 
-describe('ToolPolygonService', () => {
+fdescribe('ToolPolygonService', () => {
     let renderer2SpyObj: jasmine.SpyObj<Renderer2>;
     let toolPolygon: ToolPolygonService;
     beforeEach(() => {
@@ -39,7 +39,7 @@ describe('ToolPolygonService', () => {
         expect(shapeString).toEqual('polygon');
     });
 
-    it('#updateShape should call renderer.setAttribute with the appropriate parameters', () => {
+    it('#updateShape should call renderer.setAttribute with the appropriate parameters when the number of sides is the minimum', () => {
         const shape = {} as SVGElement;
         toolPolygon['shape'] = shape; // tslint:disable-line: no-string-literal
 
@@ -48,6 +48,40 @@ describe('ToolPolygonService', () => {
         toolPolygon['toolSettings'] = toolSettings; // tslint:disable-line: no-string-literal
 
         const pointString = '5, 0 9.330127018922195, 7.499999999999999 0.6698729810778072, 7.500000000000002 ';
+
+        const shapeArea = { x: 0, y: 0, width: 10, height: 10} as Rect;
+        const scale = { x: 1, y: 1 } as Vec2;
+
+        toolPolygon['updateShape'](shapeArea, scale, shape); // tslint:disable-line: no-string-literal
+        expect(renderer2SpyObj.setAttribute).toHaveBeenCalledWith(shape, 'points', pointString);
+    });
+
+    it('#updateShape should call renderer.setAttribute with the appropriate parameters for 6 sides', () => {
+        const shape = {} as SVGElement;
+        toolPolygon['shape'] = shape; // tslint:disable-line: no-string-literal
+
+        const toolSettings = new Map<ToolSetting, number>();
+        toolSettings.set(ToolSetting.PolygonSideCount, 6); // tslint:disable-line: no-magic-numbers
+        toolPolygon['toolSettings'] = toolSettings; // tslint:disable-line: no-string-literal
+
+        const pointString = '5, 0 9.330127018922193, 2.5 9.330127018922195, 7.499999999999999 5.000000000000002, 10 0.6698729810778072, 7.500000000000002 0.6698729810778046, 2.500000000000003 ';
+
+        const shapeArea = { x: 0, y: 0, width: 10, height: 10} as Rect;
+        const scale = { x: 1, y: 1 } as Vec2;
+
+        toolPolygon['updateShape'](shapeArea, scale, shape); // tslint:disable-line: no-string-literal
+        expect(renderer2SpyObj.setAttribute).toHaveBeenCalledWith(shape, 'points', pointString);
+    });
+
+    it('#updateShape should call renderer.setAttribute with the appropriate parameters when the number of sides is the maximum', () => {
+        const shape = {} as SVGElement;
+        toolPolygon['shape'] = shape; // tslint:disable-line: no-string-literal
+
+        const toolSettings = new Map<ToolSetting, number>();
+        toolSettings.set(ToolSetting.PolygonSideCount, 12); // tslint:disable-line: no-magic-numbers
+        toolPolygon['toolSettings'] = toolSettings; // tslint:disable-line: no-string-literal
+
+        const pointString = '5, 0 7.5, 0.6698729810778064 9.330127018922193, 2.4999999999999996 10, 4.999999999999999 9.330127018922195, 7.499999999999999 7.500000000000002, 9.330127018922193 5.000000000000003, 10 2.5000000000000027, 9.330127018922195 0.669872981077809, 7.5000000000000036 0, 5.000000000000005 0.6698729810778037, 2.500000000000005 2.499999999999994, 0.6698729810778108 ';
 
         const shapeArea = { x: 0, y: 0, width: 10, height: 10} as Rect;
         const scale = { x: 1, y: 1 } as Vec2;
