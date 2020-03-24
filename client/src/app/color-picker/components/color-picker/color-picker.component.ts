989dfed4e92e4388312bbc45a95339819344e351
Francis Pepin <francis.pepin@polymtl.ca>
2020-03-24 19:55:26 -0400

    Change variable name for consistency

--- client/src/app/color-picker/components/color-picker/color-picker.component.ts
+++ client/src/app/color-picker/components/color-picker/color-picker.component.ts
@@ -9,7 +9,7 @@ import { ColorPickerService } from '@app/color-picker/services/color-picker.serv
     providers: [ColorPickerService],
 })
 export class ColorPickerComponent {
-    @Input() previewTooltip = '';
+    @Input() colorPreviewTooltip = '';
     @Input()
     set colorModel(color: Color) {
         this.colorPickerService.setColor(color);
