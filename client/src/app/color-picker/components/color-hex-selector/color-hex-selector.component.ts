93318dc34a097e7619cf52063d54593aa37abdae
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-03-26 00:13:49 -0400

    Fix FormGroup naming in hex selector

--- client/src/app/color-picker/components/color-hex-selector/color-hex-selector.component.ts
+++ client/src/app/color-picker/components/color-hex-selector/color-hex-selector.component.ts
@@ -11,7 +11,7 @@ import { Subscription } from 'rxjs';
     styleUrls: ['./color-hex-selector.component.scss'],
 })
 export class ColorHexSelectorComponent implements OnInit, OnDestroy {
-    hexSelectorGroup = new FormGroup({
+    hexSelectorFormGroup = new FormGroup({
         hexCombinedRgb: new FormControl('000000', [Validators.required, Validators.pattern(Regexes.sixHexRegex)]),
         hexRed: new FormControl('00', [Validators.required, Validators.pattern(Regexes.twoHexRegex)]),
         hexGreen: new FormControl('00', [Validators.required, Validators.pattern(Regexes.twoHexRegex)]),
@@ -34,28 +34,28 @@ export class ColorHexSelectorComponent implements OnInit, OnDestroy {
             this.updateAll(color.getHex());
         });
 
-        this.hexCombinedRgbChangedSubscription = this.hexSelectorGroup.controls.hexCombinedRgb.valueChanges.subscribe(() => {
+        this.hexCombinedRgbChangedSubscription = this.hexSelectorFormGroup.controls.hexCombinedRgb.valueChanges.subscribe(() => {
             this.updateHexRgbComponents();
             this.updateColorPicker();
-            this.hexSelectorGroup.updateValueAndValidity({ emitEvent: false });
+            this.hexSelectorFormGroup.updateValueAndValidity({ emitEvent: false });
         });
 
-        this.hexRedChangedSubscription = this.hexSelectorGroup.controls.hexRed.valueChanges.subscribe(() => {
+        this.hexRedChangedSubscription = this.hexSelectorFormGroup.controls.hexRed.valueChanges.subscribe(() => {
             this.updateHexCombinedRgb();
             this.updateColorPicker();
-            this.hexSelectorGroup.updateValueAndValidity({ emitEvent: false });
+            this.hexSelectorFormGroup.updateValueAndValidity({ emitEvent: false });
         });
 
-        this.hexGreenChangedSubscription = this.hexSelectorGroup.controls.hexGreen.valueChanges.subscribe(() => {
+        this.hexGreenChangedSubscription = this.hexSelectorFormGroup.controls.hexGreen.valueChanges.subscribe(() => {
             this.updateHexCombinedRgb();
             this.updateColorPicker();
-            this.hexSelectorGroup.updateValueAndValidity({ emitEvent: false });
+            this.hexSelectorFormGroup.updateValueAndValidity({ emitEvent: false });
         });
 
-        this.hexBlueChangedSubscription = this.hexSelectorGroup.controls.hexBlue.valueChanges.subscribe(() => {
+        this.hexBlueChangedSubscription = this.hexSelectorFormGroup.controls.hexBlue.valueChanges.subscribe(() => {
             this.updateHexCombinedRgb();
             this.updateColorPicker();
-            this.hexSelectorGroup.updateValueAndValidity({ emitEvent: false });
+            this.hexSelectorFormGroup.updateValueAndValidity({ emitEvent: false });
         });
     }
 
@@ -70,35 +70,35 @@ export class ColorHexSelectorComponent implements OnInit, OnDestroy {
     swapMode(event: MouseEvent): void {
         this.isCombinedHex = !this.isCombinedHex;
         event.preventDefault();
-        this.hexSelectorGroup.updateValueAndValidity();
+        this.hexSelectorFormGroup.updateValueAndValidity();
     }
 
     private updateHexCombinedRgb(): void {
         const hexCombinedRgbString =
-            this.hexSelectorGroup.controls.hexRed.value +
-            this.hexSelectorGroup.controls.hexGreen.value +
-            this.hexSelectorGroup.controls.hexBlue.value;
-        this.hexSelectorGroup.controls.hexCombinedRgb.setValue(hexCombinedRgbString, { emitEvent: false });
+            this.hexSelectorFormGroup.controls.hexRed.value +
+            this.hexSelectorFormGroup.controls.hexGreen.value +
+            this.hexSelectorFormGroup.controls.hexBlue.value;
+        this.hexSelectorFormGroup.controls.hexCombinedRgb.setValue(hexCombinedRgbString, { emitEvent: false });
     }
 
     private updateHexRgbComponents(): void {
-        const hexCombinedRgbString = this.hexSelectorGroup.controls.hexCombinedRgb.value;
+        const hexCombinedRgbString = this.hexSelectorFormGroup.controls.hexCombinedRgb.value;
 
         // tslint:disable: no-magic-numbers
-        this.hexSelectorGroup.controls.hexRed.setValue(hexCombinedRgbString.substring(0, 2), { emitEvent: false });
-        this.hexSelectorGroup.controls.hexGreen.setValue(hexCombinedRgbString.substring(2, 4), { emitEvent: false });
-        this.hexSelectorGroup.controls.hexBlue.setValue(hexCombinedRgbString.substring(4, 6), { emitEvent: false });
+        this.hexSelectorFormGroup.controls.hexRed.setValue(hexCombinedRgbString.substring(0, 2), { emitEvent: false });
+        this.hexSelectorFormGroup.controls.hexGreen.setValue(hexCombinedRgbString.substring(2, 4), { emitEvent: false });
+        this.hexSelectorFormGroup.controls.hexBlue.setValue(hexCombinedRgbString.substring(4, 6), { emitEvent: false });
         // tslint:enable: no-magic-numbers
     }
 
     private updateAll(hexCombinedRgbString: string): void {
-        this.hexSelectorGroup.controls.hexCombinedRgb.setValue(hexCombinedRgbString, { emitEvent: false });
+        this.hexSelectorFormGroup.controls.hexCombinedRgb.setValue(hexCombinedRgbString, { emitEvent: false });
         this.updateHexRgbComponents();
     }
 
     private updateColorPicker(): void {
-        if (this.hexSelectorGroup.controls.hexCombinedRgb.valid) {
-            const color = Color.fromHex(this.hexSelectorGroup.controls.hexCombinedRgb.value);
+        if (this.hexSelectorFormGroup.controls.hexCombinedRgb.valid) {
+            const color = Color.fromHex(this.hexSelectorFormGroup.controls.hexCombinedRgb.value);
             color.alpha = this.colorPickerService.alpha;
             this.colorPickerService.setColor(color);
         }
