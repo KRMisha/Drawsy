9d53a6b15927789f7d0c92ca2f157b47bfdd3ca7
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-03-27 19:19:16 -0400

    Add tests for new-drawing-component

--- client/src/app/modals/components/new-drawing/new-drawing.component.spec.ts
+++ client/src/app/modals/components/new-drawing/new-drawing.component.spec.ts
@@ -1,42 +1,62 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { DrawingService } from '@app/drawing/services/drawing.service';
 import { NewDrawingComponent } from '@app/modals/components/new-drawing/new-drawing.component';
 import { Color } from '@app/shared/classes/color';
+import { ErrorMessageService } from '@app/shared/services/error-message.service';
+import { Subject } from 'rxjs';
 
 // tslint:disable: max-classes-per-file
 // tslint:disable: no-magic-numbers
 // tslint:disable: no-empty
-
-class MockDrawingService {
-    isDrawingStarted: boolean;
-    clearStoredElements = () => {};
-}
-
-// class MockWindow {
-//     innerWidth: number;
-//     innerHeight: number;
-// }
+// tslint:disable: no-any
+// tslint:disable: no-string-literal
 
 describe('NewDrawingComponent', () => {
     let component: NewDrawingComponent;
     let fixture: ComponentFixture<NewDrawingComponent>;
     let routerSpyObj: jasmine.SpyObj<Router>;
-    let drawingServiceMock: MockDrawingService;
+    let drawingServiceSpyObj: jasmine.SpyObj<DrawingService>;
+    let valueChangesSubject: Subject<any>;
+    let widthSpyObj: jasmine.SpyObj<FormControl>;
+    let heightSpyObj: jasmine.SpyObj<FormControl>;
+    let validFormGroupSpyObj: jasmine.SpyObj<FormGroup>;
+    let invalidFormGroupSpyObj: jasmine.SpyObj<FormGroup>;
+
+    const sidebarWidth = 337;
 
     beforeEach(async(() => {
         routerSpyObj = jasmine.createSpyObj('Router', ['navigate']);
-        drawingServiceMock = new MockDrawingService();
+        drawingServiceSpyObj = jasmine.createSpyObj('DrawingService', ['confirmNewDrawing']);
+        valueChangesSubject = new Subject<any>();
+        widthSpyObj = jasmine.createSpyObj('FormControl', ['setValue'], { value: 18 });
+        heightSpyObj = jasmine.createSpyObj('FormControl', ['setValue'], { value: 18 });
+        validFormGroupSpyObj = jasmine.createSpyObj('FormGroup', [], {
+            valid: true,
+            valueChanges: valueChangesSubject,
+            controls: {
+                width: widthSpyObj,
+                height: heightSpyObj,
+            },
+        });
+        invalidFormGroupSpyObj = jasmine.createSpyObj('FormGroup', [], {
+            valid: false,
+            valueChanges: valueChangesSubject,
+            controls: {
+                width: widthSpyObj,
+                height: heightSpyObj,
+            },
+        });
 
         TestBed.configureTestingModule({
             declarations: [NewDrawingComponent],
             imports: [FormsModule, MatCardModule, MatIconModule, ReactiveFormsModule],
             providers: [
-                { provide: DrawingService, useValue: drawingServiceMock },
+                { provide: DrawingService, useValue: drawingServiceSpyObj },
                 { provide: Router, useValue: routerSpyObj },
             ],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -47,91 +67,128 @@ describe('NewDrawingComponent', () => {
         fixture = TestBed.createComponent(NewDrawingComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-        component.backgroundColor = new Color();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
 
-    // START NOTE: MOVE THESE TESTS TO DRAWING SERVICE!
-
-    it('#onSubmit should create a new drawing if no drawing is started', () => {
-        spyOn(drawingServiceMock, 'clearStoredElements').and.callThrough();
-        drawingServiceMock.isDrawingStarted = false;
-
-        component.onSubmit();
-        expect(drawingServiceMock.clearStoredElements).toHaveBeenCalled();
-        expect(routerSpyObj.navigate).toHaveBeenCalled();
+    it("#ngOnInit should subscribe to the drawingFormGroup's valueChanges", () => {
+        component.drawingFormGroup = validFormGroupSpyObj;
+        component.wereDimensionsModified = false;
+        component.ngOnInit();
+        valueChangesSubject.next();
+        expect(component.wereDimensionsModified).toEqual(true);
     });
 
-    it('#onSubmit should do nothing if a drawing is started and user cancels its action', () => {
-        spyOn(window, 'confirm').and.returnValue(false);
+    it('#ngOnDestroy should unsubscribe from its subscription', () => {
+        component.ngOnInit();
+        const dimensionChangedSubscription = spyOn(component['drawingDimensionChangedSubscription'], 'unsubscribe');
+        component.ngOnDestroy();
+        expect(dimensionChangedSubscription).toHaveBeenCalled();
+    });
 
-        spyOn(drawingServiceMock, 'clearStoredElements').and.callThrough();
-        drawingServiceMock.isDrawingStarted = true;
+    it('#onResize should not change the dimensions if the dimensions have been modified by the user', () => {
+        component.drawingFormGroup = validFormGroupSpyObj;
+        component.wereDimensionsModified = true;
+        component.onResize({} as Event);
+        expect(widthSpyObj.setValue).not.toHaveBeenCalled();
+        expect(heightSpyObj.setValue).not.toHaveBeenCalled();
+    });
 
+    it('#onResize should change the dimensions if they were not previously changed by the user', () => {
+        component.drawingFormGroup = validFormGroupSpyObj;
+        component.wereDimensionsModified = false;
+        const event = ({ target: { innerWidth: 12, innerHeight: 12 } as Window } as unknown) as Event;
+        component.onResize(event);
+        expect(widthSpyObj.setValue).toHaveBeenCalledWith(12 - sidebarWidth, { emitEvent: false });
+        expect(heightSpyObj.setValue).toHaveBeenCalledWith(12, { emitEvent: false });
+    });
+
+    it('#onSubmit should forward navigate call to the router if the drawing form is valid and drawingService.confirmNewDrawing returns succesfully', () => {
+        component.drawingFormGroup = validFormGroupSpyObj;
+        drawingServiceSpyObj.confirmNewDrawing.and.returnValue(true);
         component.onSubmit();
-        expect(drawingServiceMock.clearStoredElements).not.toHaveBeenCalled();
+        expect(drawingServiceSpyObj.confirmNewDrawing).toHaveBeenCalledWith(
+            { x: 18, y: 18 },
+            Color.fromRgb(Color.maxRgb, Color.maxRgb, Color.maxRgb)
+        );
+        expect(routerSpyObj.navigate).toHaveBeenCalledWith(['/editor']);
+    });
+
+    it('#onSubmit should not forward call to the router if the drawing form is invalid', () => {
+        component.drawingFormGroup = invalidFormGroupSpyObj;
+        component.onSubmit();
         expect(routerSpyObj.navigate).not.toHaveBeenCalled();
     });
 
-    it('#onSubmit should create a new drawing if a drawing is started and user confirms its action', () => {
-        spyOn(window, 'confirm').and.returnValue(true);
-
-        spyOn(drawingServiceMock, 'clearStoredElements').and.callThrough();
-        drawingServiceMock.isDrawingStarted = true;
-
+    it("#onSubmit should not forward call to the router if drawingService's confirmNewDrawing returns false", () => {
+        component.drawingFormGroup = validFormGroupSpyObj;
+        drawingServiceSpyObj.confirmNewDrawing.and.returnValue(false);
         component.onSubmit();
-        expect(drawingServiceMock.clearStoredElements).toHaveBeenCalled();
-        expect(routerSpyObj.navigate).toHaveBeenCalled();
+        expect(routerSpyObj.navigate).not.toHaveBeenCalled();
     });
 
-    // END NOTE
+    it('#getErrorMessage should forward the call to the ErrorMessageService', () => {
+        spyOn(ErrorMessageService, 'getErrorMessage');
+        component.getErrorMessage(widthSpyObj);
+        expect(ErrorMessageService.getErrorMessage).toHaveBeenCalledWith(widthSpyObj, 'Entiers');
+    });
 
-    // it('should remember the dimensions were modified if they were modified manually', () => {
-    //     component.drawingForm.controls.width.setValue(123);
-    //     expect(component.wereDimensionsModified).toBe(true);
-
-    //     component.wereDimensionsModified = false;
-    //     component.drawingForm.controls.height.setValue(456);
-    //     expect(component.wereDimensionsModified).toBe(true);
+    // it('drawingFormGroup should contain the window size at the start', () => {
+    //     const newWindow = { innerHeight: 1200, innerWidth: 1200} as Window;
+    //     component.drawingFormGroup.reset();
+    //     expect(component.drawingFormGroup.controls.width.value).toEqual(1200 - sidebarWidth);
+    //     expect(component.drawingFormGroup.controls.height.value).toEqual(1200);
     // });
 
-    // it('should not remember the dimensions were modified if they were not modified manually', () => {
-    //     component.drawingForm.controls.width.setValue(123, { emitEvent: false });
-    //     expect(component.wereDimensionsModified).toBe(false);
+    it('drawingFormGroup should be invalid if the width is empty', () => {
+        component.drawingFormGroup.controls.width.setValue('');
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
 
-    //     component.wereDimensionsModified = false;
-    //     component.drawingForm.controls.height.setValue(456, { emitEvent: false });
-    //     expect(component.wereDimensionsModified).toBe(false);
-    // });
+    it('drawingFormGroup should be invalid if the height is empty', () => {
+        component.drawingFormGroup.controls.height.setValue('');
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
 
-    // it('#onResize should update drawing dimensions if they were never modified manually', () => {
-    //     component.drawingForm.controls.width.setValue(123);
-    //     component.drawingForm.controls.height.setValue(456);
-    //     component.wereDimensionsModified = false;
+    it('drawingFormGroup should be invalid if the width is not an integer', () => {
+        component.drawingFormGroup.controls.height.setValue('213abc');
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
 
-    //     const mockWindow = new MockWindow();
-    //     mockWindow.innerWidth = 1337;
-    //     mockWindow.innerHeight = 1337;
-    //     component.onResize({ target: (mockWindow as unknown) as EventTarget } as Event);
+    it('drawingFormGroup should be invalid if the height is not an integer', () => {
+        component.drawingFormGroup.controls.height.setValue('ajf214');
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
 
-    //     expect(component.drawingForm.controls.width.value).not.toBe(123);
-    //     expect(component.drawingForm.controls.height.value).not.toBe(456);
-    // });
+    it('drawingFormGroup should be invalid if the width is negative', () => {
+        component.drawingFormGroup.controls.height.setValue(-78);
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
 
-    // it('#onResize should not update drawing dimensions if they were modified manually', () => {
-    //     component.drawingForm.controls.width.setValue(123);
-    //     component.drawingForm.controls.height.setValue(456);
-    //     component.wereDimensionsModified = true;
+    it('drawingFormGroup should be invalid if the height is negative', () => {
+        component.drawingFormGroup.controls.height.setValue(-12);
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
 
-    //     const mockWindow = new MockWindow();
-    //     mockWindow.innerWidth = 1337;
-    //     mockWindow.innerHeight = 1337;
-    //     component.onResize({ target: (mockWindow as unknown) as EventTarget } as Event);
+    it('drawingFormGroup should be invalid if the width is 0', () => {
+        component.drawingFormGroup.controls.height.setValue(0);
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
 
-    //     expect(component.drawingForm.controls.width.value).toBe(123);
-    //     expect(component.drawingForm.controls.height.value).toBe(456);
-    // });
+    it('drawingFormGroup should be invalid if the height is 0', () => {
+        component.drawingFormGroup.controls.height.setValue(0);
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
+
+    it('drawingFormGroup should be invalid if the width is larger than 10000', () => {
+        component.drawingFormGroup.controls.height.setValue(10001);
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
+
+    it('drawingFormGroup should be invalid if the height is larger than 10000', () => {
+        component.drawingFormGroup.controls.height.setValue(11000);
+        expect(component.drawingFormGroup.valid).toEqual(false);
+    });
 });
