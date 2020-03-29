fb0e7ca8c8f56a54b5b4b502a50759c29cd4c2c3
Samer Massaad <samer.massaad@polymtl.ca>
2020-03-29 19:05:49 -0400

    Fix shortcut problems with caps-lock

--- client/src/app/shared/services/shortcut.service.ts
+++ client/src/app/shared/services/shortcut.service.ts
@@ -67,28 +67,29 @@ export class ShortcutService {
     private handleCtrlShortcuts(event: KeyboardEvent): void {
         switch (event.key) {
             case 'e':
+            case 'E':
                 event.preventDefault();
                 this.openExportDrawingShortcutSource.next();
                 break;
             case 'g':
+            case 'G':
                 event.preventDefault();
                 this.openGalleryShortcutSource.next();
                 break;
             case 'o':
+            case 'O':
                 event.preventDefault();
                 this.openNewDrawingShortcutSource.next();
                 break;
             case 's':
+            case 'S':
                 event.preventDefault();
                 this.openSaveDrawingShortcutSource.next();
                 break;
             case 'z':
-                event.preventDefault();
-                this.undoShortcutSource.next();
-                break;
             case 'Z':
                 event.preventDefault();
-                this.redoShortcutSource.next();
+                event.shiftKey ? this.redoShortcutSource.next() : this.undoShortcutSource.next();
                 break;
         }
     }
@@ -105,30 +106,39 @@ export class ShortcutService {
                 this.selectToolPolygonShortcutSource.next();
                 break;
             case 'a':
+            case 'A':
                 this.selectToolSprayCanShortcutSource.next();
                 break;
             case 'c':
+            case 'C':
                 this.selectToolPencilShortcutSource.next();
                 break;
             case 'e':
+            case 'E':
                 this.selectToolEraserShortcutSource.next();
                 break;
             case 'g':
+            case 'G':
                 this.toggleGridSource.next();
                 break;
             case 'i':
+            case 'I':
                 this.selectToolEyedropperShortcutSource.next();
                 break;
             case 'l':
+            case 'L':
                 this.selectToolLineShortcutSource.next();
                 break;
             case 'r':
+            case 'R':
                 this.selectToolRecolorShortcutSource.next();
                 break;
             case 's':
+            case 'S':
                 this.selectToolSelectionShortcutSource.next();
                 break;
             case 'w':
+            case 'W':
                 this.selectToolPaintbrushShortcutSource.next();
                 break;
             case '+':
