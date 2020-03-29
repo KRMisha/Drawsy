f8b4141168bcebc90830e7d7fc69b461e719b781
Samer Massaad <samer.massaad@polymtl.ca>
2020-03-29 01:31:03 -0400

    Add tests to hue-slider-component

--- client/src/app/color-picker/components/hue-slider/hue-slider.component.spec.ts
+++ client/src/app/color-picker/components/hue-slider/hue-slider.component.spec.ts
@@ -1,16 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HueSliderComponent } from '@app/color-picker/components/hue-slider/hue-slider.component';
 import { ColorPickerService } from '@app/color-picker/services/color-picker.service';
+import { Color } from '@app/shared/classes/color';
 import { Subject } from 'rxjs';
 
-// tslint:disable: no-magic-numbers
+// tslint:disable: no-any
 // tslint:disable: no-string-literal
 
-describe('HueSliderComponent', () => {
+const canvasWidth = 200;
+
+fdescribe('HueSliderComponent', () => {
     let component: HueSliderComponent;
     let fixture: ComponentFixture<HueSliderComponent>;
     let colorPickerServiceSpyObj: jasmine.SpyObj<ColorPickerService>;
+    let drawSpy: jasmine.Spy<any>;
+    let updateHueSpy: jasmine.Spy<any>;
+
     let hueChangedSubject: Subject<number>;
+
     beforeEach(async(() => {
         hueChangedSubject = new Subject<number>();
         colorPickerServiceSpyObj = jasmine.createSpyObj('ColorPickerService', [], {
@@ -27,43 +34,85 @@ describe('HueSliderComponent', () => {
         fixture = TestBed.createComponent(HueSliderComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-        component.onMouseEnter();
+        drawSpy = spyOn<any>(component, 'draw').and.callThrough();
+        updateHueSpy = spyOn<any>(component, 'updateHue').and.callThrough();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
 
-    // it('#onMouseDown should do nothing if mouse is not inside', () => {
-    //     component.onMouseLeave();
-    //     component.onMouseDown({ offsetX: 50 } as MouseEvent);
-    //     expect(component.hueChange.emit).not.toHaveBeenCalled();
-    // });
+    it('#ngAfterViewInit should subscribe to hue changes', () => {
+        const hueChangedSpy = spyOn(hueChangedSubject, 'subscribe').and.callThrough();
+        component.ngAfterViewInit();
+        expect(hueChangedSpy).toHaveBeenCalled();
+    });
 
-    // it('#onMouseDown should emit a hue change if mouse is inside', () => {
-    //     component.onMouseDown({ offsetX: 50 } as MouseEvent);
-    //     expect(component.hueChange.emit).toHaveBeenCalled();
-    // });
+    it("should update the slider's postion and call draw method on hue change", () => {
+        component.ngAfterViewInit();
+        const emitedValue = 1;
+        hueChangedSubject.next(emitedValue);
 
-    // it("#onMouseDown shouldn't emit a hue change if mouse is inside but the mouse is not down", () => {
-    //     component.onMouseUp();
-    //     component.onMouseMove({ offsetX: 50 } as MouseEvent);
-    //     expect(component.hueChange.emit).not.toHaveBeenCalled();
-    // });
+        const expectedSliderPosition = (emitedValue / Color.maxHue) * canvasWidth;
+        expect(component['sliderPosition']).toEqual(expectedSliderPosition);
+        expect(drawSpy).toHaveBeenCalled();
+    });
 
-    // it('#set hue should redraw the canvas', () => {
-    //     spyOn(component, 'draw');
-    //     component['canvas'] = {} as HTMLCanvasElement;
-    //     component.hue = 3;
-    //     expect(component.draw).toHaveBeenCalled();
-    // });
+    it('#ngOnDestroy should unsubscribe from hue changes', () => {
+        const hueChangesSubscriptionSpy = spyOn(component['hueChangedSubscription'], 'unsubscribe');
+        component.ngOnDestroy();
+        expect(hueChangesSubscriptionSpy).toHaveBeenCalled();
+    });
 
-    // it('#set hue with undefined canvas should do nothing', () => {
-    //     spyOn(component, 'draw');
-    //     component['canvas'] = {} as HTMLCanvasElement;
-    //     component.hue = 5;
-    //     component['canvas'] = (undefined as unknown) as HTMLCanvasElement;
-    //     component.hue = 6;
-    //     expect(component.hue).toEqual(5);
-    // });
+    it('#onMouseMove should call updateValue method if isLeftMouseButtonDown is true', () => {
+        const mouseEventStub = { clientX: 10 } as MouseEvent;
+        component['isLeftMouseButtonDown'] = true;
+        component.onMouseMove(mouseEventStub);
+        expect(updateHueSpy).toHaveBeenCalledWith(mouseEventStub);
+    });
+
+    it('#onMouseMove not should call updateValue method if isLeftMouseButtonDown is false', () => {
+        const mouseEventStub = { clientX: 10 } as MouseEvent;
+        component['isLeftMouseButtonDown'] = false;
+        component.onMouseMove(mouseEventStub);
+        expect(updateHueSpy).not.toHaveBeenCalled();
+    });
+
+    it('#onMouseDown should call updateHue method and change isLeftMouseButtonDown to true if mouse is inside', () => {
+        const mouseEventStub = { clientX: 10 } as MouseEvent;
+        component['isMouseInside'] = true;
+        component['isLeftMouseButtonDown'] = false;
+        component.onMouseDown(mouseEventStub);
+
+        expect(component['isLeftMouseButtonDown']).toEqual(true);
+        expect(updateHueSpy).toHaveBeenCalledWith(mouseEventStub);
+    });
+
+    it('#onMouseDown not should call updateHue method and change isLeftMouseButtonDown to true if mouse is not inside', () => {
+        const mouseEventStub = { clientX: 10 } as MouseEvent;
+        component['isMouseInside'] = false;
+        component['isLeftMouseButtonDown'] = false;
+        component.onMouseDown(mouseEventStub);
+
+        expect(component['isLeftMouseButtonDown']).toEqual(false);
+        expect(updateHueSpy).not.toHaveBeenCalled();
+    });
+
+    it('#onMouseUp should set isLeftMouseButtonDown to false', () => {
+        component['isLeftMouseButtonDown'] = true;
+        component.onMouseUp();
+        expect(component['isLeftMouseButtonDown']).toEqual(false);
+    });
+
+    it('#onMouseEnter should set isLeftMouseButtonDown to true', () => {
+        component['isMouseInside'] = false;
+        component.onMouseEnter();
+        expect(component['isMouseInside']).toEqual(true);
+    });
+
+    it('#onMouseEnter should set isLeftMouseButtonDown to false', () => {
+        component['isMouseInside'] = true;
+        component.onMouseLeave();
+        expect(component['isMouseInside']).toEqual(false);
+    });
 });
