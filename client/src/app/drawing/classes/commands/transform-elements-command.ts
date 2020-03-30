16ec33298a1f82a37e46a71eb13c3b26f79094af
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-03-29 21:49:42 -0400

    Rename transform in TransformElementsCommand

--- client/src/app/drawing/classes/commands/transform-elements-command.ts
+++ client/src/app/drawing/classes/commands/transform-elements-command.ts
@@ -1,14 +1,15 @@
 import { Command } from '@app/drawing/classes/commands/command';
 
 export class TransformElementsCommand implements Command {
-    private svgTransform: SVGTransform;
+    private svgTransformToRedo: SVGTransform;
+
     constructor(private elements: SVGGraphicsElement[]) {}
 
     undo(): void {
         for (const element of this.elements) {
             const lastTransformIndex = element.transform.baseVal.numberOfItems - 1;
-            if (this.svgTransform === undefined) {
-                this.svgTransform = element.transform.baseVal.getItem(lastTransformIndex);
+            if (this.svgTransformToRedo === undefined) {
+                this.svgTransformToRedo = element.transform.baseVal.getItem(lastTransformIndex);
             }
             element.transform.baseVal.removeItem(lastTransformIndex);
         }
@@ -16,7 +17,7 @@ export class TransformElementsCommand implements Command {
 
     redo(): void {
         for (const element of this.elements) {
-            element.transform.baseVal.appendItem(this.svgTransform);
+            element.transform.baseVal.appendItem(this.svgTransformToRedo);
         }
     }
 }
