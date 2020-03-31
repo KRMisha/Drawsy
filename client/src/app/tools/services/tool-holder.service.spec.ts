534435f965ba2c7bcad423c28c2bd30249c5b2b7
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-03-30 20:20:00 -0400

    Format files

--- client/src/app/tools/services/tool-holder.service.spec.ts
+++ client/src/app/tools/services/tool-holder.service.spec.ts
@@ -2,10 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { ToolHolderService } from '@app/tools/services/tool-holder.service';
 
 describe('ToolHolderService', () => {
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-        });
-    });
+    beforeEach(() => TestBed.configureTestingModule({}));
 
     it('should be created', () => {
         const service: ToolHolderService = TestBed.inject(ToolHolderService);
