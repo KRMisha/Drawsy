7cbc988aa67bbd328e2f3348384340f4051ca631
Justin van den Hanenberg <justin.van-den-hanenberg@polymtl.ca>
2020-03-31 23:25:17 -0400

    Add tests for tool-eraser-service

--- client/src/app/tools/services/tool-eraser.service.spec.ts
+++ client/src/app/tools/services/tool-eraser.service.spec.ts
@@ -4,17 +4,24 @@ import { RemoveElementsCommand } from '@app/drawing/classes/commands/remove-elem
 import { ElementSiblingPair } from '@app/drawing/classes/element-sibling-pair';
 import { CommandService } from '@app/drawing/services/command.service';
 import { DrawingService } from '@app/drawing/services/drawing.service';
+import { SvgUtilityService } from '@app/drawing/services/svg-utility.service';
+import { Color } from '@app/shared/classes/color';
+import { Rect } from '@app/shared/classes/rect';
 import { MouseButton } from '@app/shared/enums/mouse-button.enum';
 import { ToolEraserService } from '@app/tools/services/tool-eraser.service';
+import { Tool } from './tool';
 
 // tslint:disable: no-any
 // tslint:disable: no-string-literal
+// tslint:disable: no-magic-numbers
+// tslint:disable: max-line-length
 
 fdescribe('ToolEraserService', () => {
     let service: ToolEraserService;
     let renderer2SpyObj: jasmine.SpyObj<Renderer2>;
     let drawingServiceSpyObj: jasmine.SpyObj<DrawingService>;
     let commandServiceSpyObj: jasmine.SpyObj<CommandService>;
+    let svgUtilityServiceSpyObj: jasmine.SpyObj<SvgUtilityService>;
 
     const sVGGraphicsElementStub = {} as SVGGraphicsElement;
     const svgElementsInitialArray = [sVGGraphicsElementStub, sVGGraphicsElementStub, sVGGraphicsElementStub];
@@ -28,11 +35,14 @@ fdescribe('ToolEraserService', () => {
         });
 
         commandServiceSpyObj = jasmine.createSpyObj('CommandService', ['addCommand']);
+
+        svgUtilityServiceSpyObj = jasmine.createSpyObj('SvgUtilityService', ['getElementUnderAreaPixelPerfect', 'updateSvgRectFromRect']);
         TestBed.configureTestingModule({
             providers: [
                 { provide: RendererFactory2, useValue: rendererFactory2SpyObj },
                 { provide: DrawingService, useValue: drawingServiceSpyObj },
                 { provide: CommandService, useValue: commandServiceSpyObj },
+                { provide: SvgUtilityService, useValue: svgUtilityServiceSpyObj },
             ],
         });
         service = TestBed.inject(ToolEraserService);
@@ -52,7 +62,7 @@ fdescribe('ToolEraserService', () => {
         expect(setTimeoutSpy).not.toHaveBeenCalled();
     });
 
-    it('#onMouseMove should call #update after an interval when the timerId undefined', () => {
+    it('#onMouseMove should call #update after an interval when the timerId is undefined', () => {
         jasmine.clock().install();
         const updateEraserRectSpy = spyOn<any>(service, 'updateEraserRect');
         const updateSpy = spyOn(service, 'update');
@@ -68,16 +78,17 @@ fdescribe('ToolEraserService', () => {
         jasmine.clock().uninstall();
     });
 
-    // it("#onMouseDown should make a copy of the drawingService's svgElements and call #update", () => {
-    //     const updateSpy = spyOn(service, 'update');
-    //     service.onMouseDown({} as MouseEvent);
-    //     expect(service['drawingElementsCopy']).toEqual(svgElementsInitialArray);
-    //     expect(updateSpy).toHaveBeenCalled();
-    // });
+    it("#onMouseDown should make a copy of the drawingService's svgElements and call #update", () => {
+        const updateSpy = spyOn(service, 'update');
+        service.onMouseDown({ button: MouseButton.Left } as MouseEvent);
+        expect(service['drawingElementsCopy']).toEqual(svgElementsInitialArray);
+        expect(updateSpy).toHaveBeenCalled();
+    });
 
-    it('#onMouseUp should return early if there were no elements deleted during the drag', () => {
-        service['svgElementsDeletedDuringDrag'] = [];
-        expect(commandServiceSpyObj.addCommand).not.toHaveBeenCalled();
+    it('#onMouseDown should return early if the button is not the left mouse button', () => {
+        const updateSpy = spyOn(service, 'update');
+        service.onMouseDown({ button: MouseButton.Right } as MouseEvent);
+        expect(updateSpy).not.toHaveBeenCalled();
     });
 
     it('#onMouseUp should add a removeElementCommand with the elements in the right order using the commandService', () => {
@@ -98,13 +109,87 @@ fdescribe('ToolEraserService', () => {
         );
     });
 
+    it('#onMouseUp should return early if the button is not the left mouse button', () => {
+        const event = { button: MouseButton.Right } as MouseEvent;
+        service.onMouseUp(event);
+        expect(commandServiceSpyObj.addCommand).not.toHaveBeenCalled();
+    });
+
+    it('#onMouseUp should return early if the eraser did not delete anything during the drag', () => {
+        service['svgElementsDeletedDuringDrag'] = [];
+        const event = { button: MouseButton.Left } as MouseEvent;
+        service.onMouseUp(event);
+        expect(commandServiceSpyObj.addCommand).not.toHaveBeenCalled();
+    });
+
     it('#onEnter should call #updateEraserRect', () => {
         const updateEraserRectSpy = spyOn<any>(service, 'updateEraserRect');
         service.onEnter({} as MouseEvent);
         expect(updateEraserRectSpy).toHaveBeenCalled();
     });
 
-    // TESTS DE UPDATE LIKE WTFFFFFFFFFADSFAS EFB SDRFGSDFGV BSD ASE
+    it('#update should call #restoreElementUndorCursorAttributes and set svgElementUnderCursor to undefined if the elementToConsider is undefined', () => {
+        svgUtilityServiceSpyObj.getElementUnderAreaPixelPerfect.and.returnValue(undefined);
+        const restoreElementUnderCursorAttributesSpy = spyOn<any>(service, 'restoreElementUnderCursorAttributes').and.callThrough();
+        service.update();
+        expect(restoreElementUnderCursorAttributesSpy).toHaveBeenCalled();
+        expect(service['svgElementUnderCursor']).toBeUndefined();
+    });
+
+    it('#update should call #restoreElementUndorCursorAttributes, change svgElementUnderCursor and call #addRedBorderToElement if the elementToConsider is not the svgElementUnderCursor', () => {
+        const testElement = {} as SVGGraphicsElement;
+        service['svgElementUnderCursor'] = {} as SVGGraphicsElement;
+        svgUtilityServiceSpyObj.getElementUnderAreaPixelPerfect.and.returnValue(testElement);
+        const restoreElementUnderCursorAttributesSpy = spyOn<any>(service, 'restoreElementUnderCursorAttributes').and.callThrough();
+        const addRedBorderSpy = spyOn<any>(service, 'addRedBorderToElement');
+        service.update();
+        expect(restoreElementUnderCursorAttributesSpy).toHaveBeenCalled();
+        expect(addRedBorderSpy).toHaveBeenCalledWith(testElement);
+        expect(service['svgElementUnderCursor']).toBe(testElement);
+    });
+
+    it('#update should not call #restoreElementUndorCursorAttributes, should not change svgElementUnderCursor and should not call #addRedBorderToElement if the elementToConsider is the svgElementUnderCursor', () => {
+        const testElement = {} as SVGGraphicsElement;
+        svgUtilityServiceSpyObj.getElementUnderAreaPixelPerfect.and.returnValue(testElement);
+        const restoreElementUnderCursorAttributesSpy = spyOn<any>(service, 'restoreElementUnderCursorAttributes');
+        const addRedBorderSpy = spyOn<any>(service, 'addRedBorderToElement');
+        service['svgElementUnderCursor'] = testElement;
+        service.update();
+        expect(restoreElementUnderCursorAttributesSpy).not.toHaveBeenCalled();
+        expect(addRedBorderSpy).not.toHaveBeenCalled();
+    });
+
+    it("#update should add the svgElementUnderCursor to the svgElementsDeletedDuringDrag and call drawingService's removeElement if the svgElementUnderCursor is not undefined, the left mouse button is pressed inside the drawing and the element is in the drawingElementsCopy", () => {
+        const pushSpy = spyOn(service['svgElementsDeletedDuringDrag'], 'push');
+        const testElement = {} as SVGGraphicsElement;
+        const fillElement = {} as SVGGraphicsElement;
+        const drawingElementsCopy = [fillElement, fillElement, fillElement, testElement];
+        Tool.isLeftMouseButtonDown = true;
+        service['isLeftMouseButtonDownInsideDrawing'] = true;
+        svgUtilityServiceSpyObj.getElementUnderAreaPixelPerfect.and.returnValue(testElement);
+        service['svgElementUnderCursor'] = testElement;
+        service['drawingElementsCopy'] = drawingElementsCopy;
+        service.update();
+        expect(pushSpy).toHaveBeenCalled();
+        expect(drawingServiceSpyObj.removeElement).toHaveBeenCalledWith(testElement);
+        expect(service['svgElementUnderCursor']).toBeUndefined();
+    });
+
+    it("#update should not add the svgElementUnderCursor to the svgElementsDeletedDuringDrag, should not call drawingService's removeElement and should set svgElementUnderCursor to undefined if the svgElementUnderCursor not in drawingElementsCopy", () => {
+        const pushSpy = spyOn(service['svgElementsDeletedDuringDrag'], 'push');
+        const testElement = {} as SVGGraphicsElement;
+        const fillElement = {} as SVGGraphicsElement;
+        const drawingElementsCopy = [fillElement, fillElement, fillElement];
+        Tool.isLeftMouseButtonDown = true;
+        service['isLeftMouseButtonDownInsideDrawing'] = true;
+        svgUtilityServiceSpyObj.getElementUnderAreaPixelPerfect.and.returnValue(testElement);
+        service['svgElementUnderCursor'] = testElement;
+        service['drawingElementsCopy'] = drawingElementsCopy;
+        service.update();
+        expect(pushSpy).not.toHaveBeenCalled();
+        expect(drawingServiceSpyObj.removeElement).not.toHaveBeenCalled();
+        expect(service['svgElementUnderCursor']).toBeUndefined();
+    });
 
     it("#onToolSelection should create the eraser's rect, add it to the ui elements of drawingService and call #updateEraserRect", () => {
         const updateEraserRectSpy = spyOn<any>(service, 'updateEraserRect');
@@ -121,5 +206,62 @@ fdescribe('ToolEraserService', () => {
         expect(updateEraserRectSpy);
     });
 
-    it('#onToolDeselection should remove the ');
+    it("#onToolDeselection should call drawingService's removeUiElement, call #restoreElementUnderCursorAttributes and set svgElementUnderCursor to undefined", () => {
+        service['svgElementUnderCursor'] = {} as SVGGraphicsElement;
+        const restoreSpy = spyOn<any>(service, 'restoreElementUnderCursorAttributes');
+        service.onToolDeselection();
+        expect(drawingServiceSpyObj.removeUiElement).toHaveBeenCalled();
+        expect(restoreSpy).toHaveBeenCalled();
+        expect(service['svgElementUnderCursor']).toBeUndefined();
+    });
+
+    it("#updateEraserRect should update the eraserSize and eraserRect with the new values from the settings and call svgUtilityService's updateSvgRectFromRect", () => {
+        const expectedValue = 50;
+        service['settings'].eraserSize = expectedValue;
+        const unwantedEraserRect = {} as Rect;
+        service['eraserRect'] = unwantedEraserRect;
+        service['updateEraserRect']();
+        expect(service['eraserSize']).toEqual(expectedValue);
+        expect(service['eraserRect']).not.toEqual(unwantedEraserRect);
+        expect(svgUtilityServiceSpyObj.updateSvgRectFromRect).toHaveBeenCalled();
+    });
+
+    it('#addRedBorderToElement should use default values to call renderer\'s setAttribute when the attributs of the element are "none"', () => {
+        spyOn(Color, 'fromRgbaString').and.returnValue({ red: 255, green: 255, blue: 255, alpha: 1 } as Color);
+        const elementToSendSpy = jasmine.createSpyObj('SVGCircleElement', ['getAttribute']);
+        elementToSendSpy.getAttribute.and.returnValue('none');
+        service['addRedBorderToElement'](elementToSendSpy);
+
+        expect(renderer2SpyObj.setAttribute).toHaveBeenCalledWith(elementToSendSpy, 'stroke', 'rgb(255, 0, 0)');
+        expect(renderer2SpyObj.setAttribute).toHaveBeenCalledWith(elementToSendSpy, 'stroke-width', '3');
+    });
+
+    it('#addRedBorderToElement should put default border width to 3 if object doesn not have a border initially', () => {
+        spyOn(Color, 'fromRgbaString').and.returnValue({ red: 255, green: 255, blue: 255, alpha: 1 } as Color);
+        const elementToSendSpy = jasmine.createSpyObj('SVGCircleElement', ['getAttribute']);
+        elementToSendSpy.getAttribute.and.returnValue(undefined);
+        service['addRedBorderToElement'](elementToSendSpy);
+
+        expect(renderer2SpyObj.setAttribute).toHaveBeenCalledWith(elementToSendSpy, 'stroke', 'rgb(255, 0, 0)');
+        expect(renderer2SpyObj.setAttribute).toHaveBeenCalledWith(elementToSendSpy, 'stroke-width', '3');
+    });
+
+    it('#addRedBorderToElement should put a bigger stroke width when object is selected with eraser', () => {
+        spyOn(Color, 'fromRgbaString').and.returnValue({ red: 255, green: 255, blue: 255, alpha: 1 } as Color);
+        const elementToSendSpy = jasmine.createSpyObj('SVGCircleElement', ['getAttribute']);
+        elementToSendSpy.getAttribute.and.returnValue('123');
+        service['addRedBorderToElement'](elementToSendSpy);
+
+        expect(renderer2SpyObj.setAttribute).toHaveBeenCalledWith(elementToSendSpy, 'stroke', 'rgb(255, 0, 0)');
+        expect(renderer2SpyObj.setAttribute).toHaveBeenCalledWith(elementToSendSpy, 'stroke-width', '135.84');
+    });
+
+    it('#addRedBorderToElement should put a dark red border if color of object is red', () => {
+        spyOn(Color, 'fromRgbaString').and.returnValue({ red: 255, green: 0, blue: 0, alpha: 1 } as Color);
+        const elementToSendSpy = jasmine.createSpyObj('SVGCircleElement', ['getAttribute']);
+        elementToSendSpy.getAttribute.and.returnValue('123');
+        service['addRedBorderToElement'](elementToSendSpy);
+
+        expect(renderer2SpyObj.setAttribute).toHaveBeenCalledWith(elementToSendSpy, 'stroke', 'rgb(117, 30, 33)');
+    });
 });
