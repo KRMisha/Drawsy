04f5729c8080f2ac2db731aea050a9e80acc8a85
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-04-09 02:56:48 -0400

    Remove injectable from static service

--- client/src/app/tools/services/selection/tool-selection-transform.service.ts
+++ client/src/app/tools/services/selection/tool-selection-transform.service.ts
@@ -1,8 +1,3 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-    providedIn: 'root',
-})
 export class ToolSelectionTransformService {
     static getElementListTransformsCopy(elements: SVGGraphicsElement[], drawingRoot: SVGSVGElement): SVGTransform[][] {
         const selectedElementTransformsCopy: SVGTransform[][] = [];
