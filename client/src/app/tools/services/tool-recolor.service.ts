61faa6a7d9e00812cdaa33d69dfe85e02a645f38
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-04-12 15:11:21 -0400

    Prefer if to early return for conditional execution

--- client/src/app/tools/services/tool-recolor.service.ts
+++ client/src/app/tools/services/tool-recolor.service.ts
@@ -65,10 +65,8 @@ export class ToolRecolorService extends Tool {
         const strokeAfter = element.getAttribute('stroke') || undefined;
         const fillAfter = element.getAttribute('fill') || undefined;
 
-        if (strokeBefore === strokeAfter && fillBefore === fillAfter) {
-            return;
+        if (strokeBefore !== strokeAfter || fillBefore !== fillAfter) {
+            this.historyService.addCommand(new RecolorCommand(element, strokeBefore, fillBefore, strokeAfter, fillAfter));
         }
-
-        this.historyService.addCommand(new RecolorCommand(element, strokeBefore, fillBefore, strokeAfter, fillAfter));
     }
 }
