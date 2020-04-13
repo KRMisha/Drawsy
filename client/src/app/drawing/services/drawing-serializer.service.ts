6f666a8063e19c6491bfc4d7a8d5fcd327c2b5ee
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-04-13 03:14:34 -0400

    Refactor DrawingSerializer to remove DrawingService dependency

--- client/src/app/drawing/services/drawing-serializer.service.ts
+++ client/src/app/drawing/services/drawing-serializer.service.ts
@@ -1,11 +1,9 @@
 import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 import { DrawingLoadOptions } from '@app/drawing/classes/drawing-load-options';
 import { FileType } from '@app/drawing/enums/file-type.enum';
-import { DrawingService } from '@app/drawing/services/drawing.service';
 import { RasterizationService } from '@app/drawing/services/rasterization.service';
 import { Color } from '@app/shared/classes/color';
 import { SvgFileContainer } from '@app/shared/classes/svg-file-container';
-import { SavedFile } from '@common/communication/saved-file';
 
 @Injectable({
     providedIn: 'root',
@@ -13,23 +11,28 @@ import { SavedFile } from '@common/communication/saved-file';
 export class DrawingSerializerService {
     private renderer: Renderer2;
 
-    constructor(
-        rendererFactory: RendererFactory2,
-        private drawingService: DrawingService,
-        private rasterizationService: RasterizationService
-    ) {
+    constructor(rendererFactory: RendererFactory2, private rasterizationService: RasterizationService) {
         this.renderer = rendererFactory.createRenderer(null, null);
     }
 
-    makeSvgFileContainerFromSavedFile(savedFile: SavedFile): SvgFileContainer {
-        const svgFileContainer = this.makeSvgFileContainerFromString(savedFile.content);
-        svgFileContainer.id = savedFile.id;
-        return svgFileContainer;
+    serializeDrawing(drawingRoot: SVGSVGElement): string {
+        const xmlSerializer = new XMLSerializer();
+        return xmlSerializer.serializeToString(drawingRoot);
     }
 
-    loadDrawing(svgFileContainer: SvgFileContainer): boolean {
+    deserializeDrawing(serializedDrawingRoot: string, id?: string): SvgFileContainer {
+        const domParser = new DOMParser();
+        const document = domParser.parseFromString(serializedDrawingRoot, 'image/svg+xml');
+        const drawingRoot = document.getElementsByTagName('svg')[0];
+        const title = drawingRoot.getElementsByTagName('title')[0].innerHTML;
+        const labelsString = drawingRoot.getElementsByTagName('desc')[0].innerHTML;
+        const labels = labelsString.length === 0 ? [] : labelsString.split(',');
+        return { id, title, labels, drawingRoot } as SvgFileContainer;
+    }
+
+    getDrawingLoadOptions(svgFileContainer: SvgFileContainer): DrawingLoadOptions {
         const backgroundRectFillString = svgFileContainer.drawingRoot.getElementsByTagName('rect')[0].getAttribute('fill');
-        const backgroundRectFill = Color.fromRgbaString(backgroundRectFillString || 'rgb(255, 255, 255)');
+        const backgroundRectFill = Color.fromRgbaString(backgroundRectFillString ?? 'rgb(255, 255, 255)');
 
         const svgDrawingContent = svgFileContainer.drawingRoot.getElementsByTagName('g')[0];
         const elements: SVGGraphicsElement[] = [];
@@ -47,8 +50,7 @@ export class DrawingSerializerService {
                 elements,
             },
         };
-
-        return this.drawingService.loadDrawing(drawingLoadOptions);
+        return drawingLoadOptions;
     }
 
     exportDrawing(drawingRoot: SVGSVGElement, filename: string, fileType: FileType): void {
@@ -57,22 +59,11 @@ export class DrawingSerializerService {
             : this.exportRasterDrawing(drawingRoot, filename, fileType);
     }
 
-    private makeSvgFileContainerFromString(content: string): SvgFileContainer {
-        const domParser = new DOMParser();
-        const document = domParser.parseFromString(content, 'image/svg+xml');
-        const drawingRoot = document.getElementsByTagName('svg')[0];
-        const title = drawingRoot.getElementsByTagName('title')[0].innerHTML;
-        const labelsString = drawingRoot.getElementsByTagName('desc')[0].innerHTML;
-        const labels = labelsString.length === 0 ? [] : labelsString.split(',');
-        return { id: '', title, labels, drawingRoot } as SvgFileContainer;
-    }
-
     private exportVectorDrawing(drawingRoot: SVGSVGElement, filename: string): void {
         const fileExtension = '.svg';
 
-        const xmlSerializer = new XMLSerializer();
-        const content = xmlSerializer.serializeToString(drawingRoot);
-        const blob = new Blob([content], { type: 'image/svg+xml' });
+        const serializedDrawing = this.serializeDrawing(drawingRoot);
+        const blob = new Blob([serializedDrawing], { type: 'image/svg+xml' });
 
         const link = this.renderer.createElement('a');
         link.download = filename + fileExtension;
