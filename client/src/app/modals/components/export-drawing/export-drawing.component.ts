dfe84654b02ec981d4010c13d983f8d0a108c8ea
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-04-14 20:25:35 -0400

    Refactor form controls for sending drawing by email

--- client/src/app/modals/components/export-drawing/export-drawing.component.ts
+++ client/src/app/modals/components/export-drawing/export-drawing.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { DrawingPreviewComponent } from '@app/drawing/components/drawing-preview/drawing-preview.component';
 import { DrawingFilter } from '@app/drawing/enums/drawing-filter.enum';
 import { FileType } from '@app/drawing/enums/file-type.enum';
@@ -8,30 +8,34 @@ import { ExportDrawingService } from '@app/modals/services/export-drawing.servic
 import { ErrorMessageService } from '@app/shared/services/error-message.service';
 import EmailValidation from '@common/validation/email-validation';
 import MetadataValidation from '@common/validation/metadata-validation';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-export-drawing',
     templateUrl: './export-drawing.component.html',
     styleUrls: ['./export-drawing.component.scss'],
 })
-export class ExportDrawingComponent {
+export class ExportDrawingComponent implements OnInit, OnDestroy {
     // Make enums available to template
     DrawingFilter = DrawingFilter;
     FileType = FileType;
 
     drawingFilter = DrawingFilter.None;
     fileType = FileType.Svg;
-    isSentByEmail = false;
 
-    titleFormControl = new FormControl(this.drawingService.title, [
-        Validators.required,
-        Validators.pattern(MetadataValidation.contentRegex),
-        Validators.maxLength(MetadataValidation.maxTitleLength),
-    ]);
-
-    emailFormControl = new FormControl('', [Validators.required, Validators.pattern(EmailValidation.emailRegex)]);
+    exportDrawingFormGroup = new FormGroup({
+        title: new FormControl(this.drawingService.title, [
+            Validators.required,
+            Validators.pattern(MetadataValidation.contentRegex),
+            Validators.maxLength(MetadataValidation.maxTitleLength),
+        ]),
+        emailEnabled: new FormControl(false),
+        emailAddress: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.pattern(EmailValidation.emailRegex)]),
+    });
 
     @ViewChild('appDrawingPreview') private drawingPreview: DrawingPreviewComponent;
+
+    private emailEnabledChangedSubscription: Subscription;
 
     constructor(
         private changeDetectorRef: ChangeDetectorRef,
@@ -39,20 +43,28 @@ export class ExportDrawingComponent {
         private drawingService: DrawingService
     ) {}
 
+    ngOnInit(): void {
+        this.emailEnabledChangedSubscription = this.exportDrawingFormGroup.controls.emailEnabled.valueChanges.subscribe(() => {
+            this.exportDrawingFormGroup.controls.emailEnabled.value
+                ? this.exportDrawingFormGroup.controls.emailAddress.enable()
+                : this.exportDrawingFormGroup.controls.emailAddress.disable();
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.emailEnabledChangedSubscription.unsubscribe();
+    }
+
     onSubmit(): void {
-        this.drawingService.title = this.titleFormControl.value;
+        this.drawingService.title = this.exportDrawingFormGroup.controls.title.value;
         this.changeDetectorRef.detectChanges();
         const drawingRoot = this.drawingPreview.drawingRoot.nativeElement;
-        this.isSentByEmail
-            ? this.exportDrawingService.emailDrawing(drawingRoot, this.emailFormControl.value, this.fileType)
+        this.exportDrawingFormGroup.controls.emailEnabled.value
+            ? this.exportDrawingService.emailDrawing(drawingRoot, this.exportDrawingFormGroup.controls.emailAddress.value, this.fileType)
             : this.exportDrawingService.downloadDrawing(drawingRoot, this.fileType);
     }
 
-    getErrorMessage(): string {
-        return ErrorMessageService.getErrorMessage(this.titleFormControl, 'A-Z, a-z, 0-9');
-    }
-
-    get isInformationValid(): boolean {
-        return this.isSentByEmail ? this.titleFormControl.valid && this.emailFormControl.valid : this.titleFormControl.valid;
+    getErrorMessage(formControl: AbstractControl, humanFriendlyPattern?: string): string {
+        return ErrorMessageService.getErrorMessage(formControl, humanFriendlyPattern);
     }
 }
