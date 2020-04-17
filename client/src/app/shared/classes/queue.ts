5c9a3ce32bcb7c2764e60ab1be69d00ce05f8622
Misha Krieger-Raynauld <misha.krieger-raynauld@polymtl.ca>
2020-04-17 17:41:48 -0400

    Simplify Queue

--- client/src/app/shared/classes/queue.ts
+++ client/src/app/shared/classes/queue.ts
@@ -1,36 +1,37 @@
 export class Queue<T> {
-    private count = 0;
-    private lowestCount = 0;
-    private storage: T[] = [];
+    private firstIndex = 0;
+    private elements: T[] = [];
 
     enqueue(value: T): void {
         if (value === undefined) {
             return;
         }
-        this.storage[this.count++] = value;
+        this.elements.push(value);
     }
 
     dequeue(): T | undefined {
-        if (this.count === this.lowestCount) {
+        if (this.firstIndex >= this.elements.length) {
             return undefined;
         }
-        const result = this.storage[this.lowestCount];
-        delete this.storage[this.lowestCount++];
+
+        const result = this.elements[this.firstIndex];
+        delete this.elements[this.firstIndex++];
         return result;
     }
 
     peek(): T | undefined {
-        if (this.count === this.lowestCount) {
+        if (this.firstIndex >= this.elements.length) {
             return undefined;
         }
-        return this.storage[this.lowestCount];
+
+        return this.elements[this.firstIndex];
     }
 
     isEmpty(): boolean {
-        return this.lowestCount === this.count;
+        return this.firstIndex === this.elements.length;
     }
 
     size(): number {
-        return this.count - this.lowestCount;
+        return this.elements.length - this.firstIndex;
     }
 }
